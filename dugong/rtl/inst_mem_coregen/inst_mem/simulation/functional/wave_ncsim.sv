36a9cac484ebc8e7c800941c232fe794dccdc881
Matthew Bridges <matthewbridges88@gmail.com>
2013-03-27 09:08:23 +0200

    recreates Instruction Memory Coregen

---
+++ dugong/rtl/inst_mem_coregen/inst_mem/simulation/functional/wave_ncsim.sv
@@ -0,0 +1,18 @@
+
+
+ 
+ 
+ 
+
+
+
+
+window new WaveWindow  -name  "Waves for BMG Example Design"
+waveform  using  "Waves for BMG Example Design"
+
+      waveform add -signals /inst_mem_tb/status
+      waveform add -signals /inst_mem_tb/inst_mem_synth_inst/bmg_port/CLKA
+      waveform add -signals /inst_mem_tb/inst_mem_synth_inst/bmg_port/ADDRA
+      waveform add -signals /inst_mem_tb/inst_mem_synth_inst/bmg_port/DOUTA
+
+console submit -using simulator -wait no "run"
