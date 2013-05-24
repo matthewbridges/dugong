db252e580e113b9e8904061bcec0bb5649375c2e
Matthew Bridges <matthewbridges88@gmail.com>
2013-05-24 12:19:36 +0200

    Updates header of BRAM_SYNC_SP

--- DUGONG_IP_CORE_Lib/wb_bus/rtl/bram_sync_dp.vhd
+++ DUGONG_IP_CORE_Lib/wb_bus/rtl/bram_sync_dp.vhd
@@ -18,12 +18,13 @@
 -- Company:			UNIVERSITY OF CAPE TOWN
 -- Engineer:		MATTHEW BRIDGES
 --
--- Name:			BRAM_SYNC_DP (006)
+-- Name:			BRAM_SYNC_DP (005)
 -- Type:			PRIMITIVE (2)
--- Description:		An IP core containing BRAM/s which can be used for temporary storage of general data	
+-- Description:		A BRAM primitive with one port which can take on generic data and address widths. Takes 
+--					advantage of FPGA on chip BRAMs
 --
 -- Compliance:		DUGONG V1.0 (1-0)
--- ID:				x 1-0-2-006
+-- ID:				x 1-0-2-005
 ---------------------------------------------------------------------------------------------------------------
 
 library IEEE;
