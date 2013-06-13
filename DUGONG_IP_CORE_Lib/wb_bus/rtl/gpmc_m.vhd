8fcb4127423191f6d77204ae2fedfee5ce07ca98
Matthew Bridges <matthewbridges88@gmail.com>
2013-06-13 14:11:27 +0200

    Adds working version of GPMC to WB bridge. Testbench included

--- DUGONG_IP_CORE_Lib/wb_bus/rtl/gpmc_m.vhd
+++ DUGONG_IP_CORE_Lib/wb_bus/rtl/gpmc_m.vhd
@@ -35,15 +35,24 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity gpmc_m is
 	generic(
-		DATA_WIDTH : natural := 32;
-		ADDR_WIDTH : natural := 28
+		DATA_WIDTH      : natural := 32;
+		ADDR_WIDTH      : natural := 12;
+		GPMC_ADDR_WIDTH : natural := 28
 	);
 	port(
-		GPMC_CLK_I      : in    STD_LOGIC;
-		--Master to WB
-		WB_MS           : out   STD_LOGIC_VECTOR(2 + ADDR_WIDTH + DATA_WIDTH downto 0);
-		WB_SM           : in    STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
+		--System Control Inputs
+		CLK_I           : in    STD_LOGIC;
+		RST_I           : in    STD_LOGIC;
+		--Wishbone Master Lines
+		ADR_O           : out   STD_LOGIC_VECTOR(ADDR_WIDTH - 1 downto 0);
+		DAT_I           : in    STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
+		DAT_O           : out   STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
+		WE_O            : out   STD_LOGIC;
+		STB_O           : out   STD_LOGIC;
+		ACK_I           : in    STD_LOGIC;
+		CYC_O           : out   STD_LOGIC;
 		--GPMC Interface
+		GPMC_CLK_I      : in    STD_LOGIC;
 		GPMC_D_B        : inout STD_LOGIC_VECTOR(15 downto 0);
 		GPMC_A_I        : in    STD_LOGIC_VECTOR(10 downto 1);
 		GPMC_nCS_I      : in    STD_LOGIC_VECTOR(6 downto 0);
@@ -54,52 +63,103 @@ entity gpmc_m is
 end gpmc_m;
 
 architecture Behavioral of gpmc_m is
+	--WB Master Lines
+	signal adr_ms : std_logic_vector(GPMC_ADDR_WIDTH - 1 downto 0);
+	signal dat_sm : std_logic_vector(DATA_WIDTH - 1 downto 0);
 	signal dat_ms : std_logic_vector(DATA_WIDTH - 1 downto 0);
---	alias dat_sm  : std_logic_vector(DATA_WIDTH - 1 downto 0) is WB_SM(DATA_WIDTH - 1 downto 0);
-	signal adr_ms : std_logic_vector(ADDR_WIDTH - 1 downto 0);
 	signal we_ms  : std_logic;
+	--signal sel_ms : stb_logic_vector(? downto 0);
 	signal stb_ms : std_logic;
 	signal cyc_ms : std_logic;
 
 	signal gpmc_dout : std_logic_vector(15 downto 0);
 
-	signal gpmc_sel : std_logic;
-
-	signal selected : std_logic;
+	signal word_sel : std_logic;
+	signal gpmc_stb : std_logic;
+	signal gpmc_ack : std_logic;
 
 begin
-	selected <= '0' when (GPMC_nCS_I = "1111111") else '1';
-
-	process(GPMC_CLK_I)
+	process(CLK_I)
 	begin
 		--Perform Clock Rising Edge operations
-		if (rising_edge(GPMC_CLK_I)) then
-			--First cycle of the bus transaction record the address
-			if (GPMC_nADV_ALE_I = '0') then
-				gpmc_sel                        <= GPMC_D_B(0);
-				adr_ms(ADDR_WIDTH - 4 downto 0) <= GPMC_A_I & GPMC_D_B(15 downto 1);
-				case (GPMC_nCS_I) is
-					when "1111110" => adr_ms(ADDR_WIDTH - 1 downto ADDR_WIDTH - 3) <= "000"; --0x00000000
-					when "1111101" => adr_ms(ADDR_WIDTH - 1 downto ADDR_WIDTH - 3) <= "001"; --0x08000000
-					when "1111011" => adr_ms(ADDR_WIDTH - 1 downto ADDR_WIDTH - 3) <= "010"; --0x10000000
-					when "1110111" => adr_ms(ADDR_WIDTH - 1 downto ADDR_WIDTH - 3) <= "011"; --0x18000000
-					when "1101111" => adr_ms(ADDR_WIDTH - 1 downto ADDR_WIDTH - 3) <= "100"; --0x20000000
-					when "1011111" => adr_ms(ADDR_WIDTH - 1 downto ADDR_WIDTH - 3) <= "101"; --0x28000000
-					when "0111111" => adr_ms(ADDR_WIDTH - 1 downto ADDR_WIDTH - 3) <= "111"; --0x38000000
-					when others    => adr_ms(ADDR_WIDTH - 1 downto ADDR_WIDTH - 3) <= "110"; --0x30000000
-				end case;
+		if (rising_edge(CLK_I)) then
+			--RST STATE
+			if (RST_I = '1') then
+				we_ms    <= '0';
+				stb_ms   <= '0';
+				cyc_ms   <= '0';
+				gpmc_ack <= '0';
+			else
+				if (gpmc_ack = '1') then
+					gpmc_ack <= gpmc_stb;
+				elsif (stb_ms = '1') then
+					if (ACK_I = '1') then
+						dat_sm <= DAT_I;
+						we_ms  <= '0';
+						stb_ms <= '0';
+						cyc_ms <= '0';
+					end if;
+				elsif (gpmc_stb = '1') then
+					stb_ms   <= '1';
+					cyc_ms   <= '1';
+					we_ms    <= word_sel;
+					gpmc_ack <= '1';
+				end if;
 			end if;
 		end if;
 	end process;
 
-	dat_ms(31 downto 16) <= GPMC_D_B when gpmc_sel = '1' else (others => '0');
-	dat_ms(15 downto 0)  <= GPMC_D_B when gpmc_sel = '0' else (others => '0');
+	process(GPMC_CLK_I, RST_I)
+	begin
+		if (RST_I = '1') then
+			gpmc_stb <= '0';
+			word_sel <= '0';
+			adr_ms   <= (others => '0');
+			dat_ms   <= (others => '0');
+		else
+			--Perform Clock Rising Edge operations
+			if (rising_edge(GPMC_CLK_I)) then
+				if (GPMC_nCS_I /= "1111111") then
+					--First cycle of the bus transaction record the address
+					if (GPMC_nADV_ALE_I = '0') then
+						word_sel                             <= GPMC_D_B(0);
+						adr_ms(GPMC_ADDR_WIDTH - 4 downto 0) <= GPMC_A_I & GPMC_D_B(15 downto 1);
+						case (GPMC_nCS_I) is
+							when "1111110" => adr_ms(GPMC_ADDR_WIDTH - 1 downto GPMC_ADDR_WIDTH - 3) <= "000"; --0x00000000
+							when "1111101" => adr_ms(GPMC_ADDR_WIDTH - 1 downto GPMC_ADDR_WIDTH - 3) <= "001"; --0x08000000
+							when "1111011" => adr_ms(GPMC_ADDR_WIDTH - 1 downto GPMC_ADDR_WIDTH - 3) <= "010"; --0x10000000
+							when "1110111" => adr_ms(GPMC_ADDR_WIDTH - 1 downto GPMC_ADDR_WIDTH - 3) <= "011"; --0x18000000
+							when "1101111" => adr_ms(GPMC_ADDR_WIDTH - 1 downto GPMC_ADDR_WIDTH - 3) <= "100"; --0x20000000
+							when "1011111" => adr_ms(GPMC_ADDR_WIDTH - 1 downto GPMC_ADDR_WIDTH - 3) <= "101"; --0x28000000
+							when "0111111" => adr_ms(GPMC_ADDR_WIDTH - 1 downto GPMC_ADDR_WIDTH - 3) <= "111"; --0x38000000
+							when others    => adr_ms(GPMC_ADDR_WIDTH - 1 downto GPMC_ADDR_WIDTH - 3) <= "110"; --0x30000000
+						end case;
+						gpmc_stb <= not GPMC_D_B(0);
+					elsif (GPMC_nWE_I = '0') then
+						if (word_sel = '1') then
+							dat_ms(31 downto 16) <= GPMC_D_B;
+							gpmc_stb             <= '1';
+						else
+							dat_ms(15 downto 0) <= GPMC_D_B;
+						end if;
+					end if;
 
-	gpmc_dout <= "00" & adr_ms(27 downto 14) when gpmc_sel = '1' else adr_ms(13 downto 0) & "00";
+				end if;
 
-	we_ms <= not GPMC_nWE_I;
+				if (gpmc_ack = '1') then
+					gpmc_stb <= '0';
+				end if;
+			end if;
+		end if;
+	end process;
 
-	WB_MS <= cyc_ms & we_ms & stb_ms & adr_ms & dat_ms;
+	ADR_O <= adr_ms(ADDR_WIDTH - 1 downto 0);
+	DAT_O <= dat_ms;
+	WE_O  <= we_ms;
+	STB_O <= stb_ms;
+	CYC_O <= cyc_ms;
+
+	gpmc_dout <= dat_sm(31 downto 16) when word_sel = '1' else dat_sm(15 downto 0);
 
 	--GPMC tri-state buffers for GPMC Bidirectional Data Bus
 	GPMC_D_B <= gpmc_dout when GPMC_nOE_I = '0' else (others => 'Z');
