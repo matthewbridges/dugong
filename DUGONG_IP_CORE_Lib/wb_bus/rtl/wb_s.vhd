65055f4bc9feb5180aa00b393da460a1a264ff69
Matthew Bridges <matthewbridges88@gmail.com>
2013-06-19 10:42:15 +0200

    Applies changes required to get multiple Master system working

--- DUGONG_IP_CORE_Lib/wb_bus/rtl/wb_s.vhd
+++ DUGONG_IP_CORE_Lib/wb_bus/rtl/wb_s.vhd
@@ -1,18 +1,20 @@
---  
 --                    
--- _____/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\_______      
---\____/\\\///////\\\____\/\\\_______\/\\\___\/////\\\///____\/\\\\\\___\/\\\_______/\\\///\\\_____\
--- \___\/\\\_____\/\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\/\\\__\/\\\_____/\\\/__\///\\\___\    
---  \___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\   
---   \___\/\\\//////\\\_____\/\\\/////////\\\_______\/\\\_______\/\\\\//\\\\/\\\___\/\\\_______\/\\\__\  
---    \___\/\\\____\//\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\_\//\\\/\\\___\//\\\______/\\\___\
---     \___\/\\\_____\//\\\___\/\\\_______\/\\\_______\/\\\_______\/\\\__\//\\\\\\____\///\\\__/\\\_____\
---      \___\/\\\______\//\\\__\/\\\_______\/\\\____/\\\\\\\\\\\___\/\\\___\//\\\\\______\///\\\\\/______\
---       \___\///________\///___\///________\///____\///////////____\///_____\/////_________\/////________\
---        \                                                                                                \
---         \==============  Reconfigurable Hardware Interface for computatioN and radiO  ===================\
---          \============================  http://www.rhinoplatform.org  ====================================\
---           \================================================================================================\
+-- ______/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\_________     
+-- \ ____/\\\///////\\\____\/\\\_______\/\\\___\/////\\\///____\/\\\\\\___\/\\\_______/\\\///\\\_____\
+--  \ ___\/\\\_____\/\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\/\\\__\/\\\_____/\\\/__\///\\\___\    
+--   \ ___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\   
+--    \ ___\/\\\//////\\\_____\/\\\/////////\\\_______\/\\\_______\/\\\\//\\\\/\\\___\/\\\_______\/\\\__\  
+--     \ ___\/\\\____\//\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\_\//\\\/\\\___\//\\\______/\\\___\
+--      \ ___\/\\\_____\//\\\___\/\\\_______\/\\\_______\/\\\_______\/\\\__\//\\\\\\____\///\\\__/\\\_____\
+--       \ ___\/\\\______\//\\\__\/\\\_______\/\\\____/\\\\\\\\\\\___\/\\\___\//\\\\\______\///\\\\\/______\
+--        \ ___\///________\///___\///________\///____\///////////____\///_____\/////_________\/////________\
+--         \ __________________________________________\          \__________________________________________\
+--          |:------------------------------------------|: DUGONG :|-----------------------------------------:|
+--         / ==========================================/          /========================================= /
+--        / =============================================================================================== /
+--       / ================  Reconfigurable Hardware Interface for computatioN and radiO  ================ /
+--      / ===============================  http://www.rhinoplatform.org  ================================ /
+--     / =============================================================================================== /
 --
 ---------------------------------------------------------------------------------------------------------------
 -- Company:		UNIVERSITY OF CAPE TOWN
@@ -24,8 +26,8 @@
 --			take on generic data and address widths and a generic Address. DATA_WIDTH and ADDRESS_WIDTH
 --			should be set by the top level module and coincide with the widths of the Master.
 -- 
--- Compliance:		DUGONG V1.1 (1-1)
--- ID:			x 1-1-2-002
+-- Compliance:		DUGONG V1.4
+-- ID:			x 1-4-2-002
 ---------------------------------------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -34,11 +36,10 @@ use IEEE.NUMERIC_STD.ALL;
 library DUGONG_IP_CORE_Lib;
 use DUGONG_IP_CORE_Lib.dprimitives.ALL;
 
+--NB The DATA_WIDTH and ADDR_WIDTH constants are set in the dprimitives package
 entity wb_s is
 	generic(
-		DATA_WIDTH      : NATURAL               := 32;
-		ADDR_WIDTH      : NATURAL               := 12;
-		BASE_ADDR       : UNSIGNED(11 downto 0) := x"000";
+		BASE_ADDR       : UNSIGNED(15 downto 0) := x"0000";
 		CORE_DATA_WIDTH : NATURAL               := 16;
 		CORE_ADDR_WIDTH : NATURAL               := 3
 	);
@@ -47,8 +48,8 @@ entity wb_s is
 		CLK_I : in  STD_LOGIC;
 		RST_I : in  STD_LOGIC;
 		--Slave to WB
-		WB_I  : in  STD_LOGIC_VECTOR(2 + ADDR_WIDTH + DATA_WIDTH downto 0);
-		WB_O  : out STD_LOGIC_VECTOR(DATA_WIDTH downto 0);
+		WB_MS : in  WB_MS_type;
+		WB_SM : out WB_SM_type;
 		--Wishbone Slave Lines (inverted)
 		ADR_I : out STD_LOGIC_VECTOR(CORE_ADDR_WIDTH - 1 downto 0);
 		DAT_I : out STD_LOGIC_VECTOR(CORE_DATA_WIDTH - 1 downto 0);
@@ -62,12 +63,12 @@ end wb_s;
 
 architecture Behavioral of wb_s is
 	--WB
-	alias adr_ms  : std_logic_vector(ADDR_WIDTH - 1 downto 0) is WB_I(ADDR_WIDTH - 1 downto 0);
-	alias dat_ms  : std_logic_vector(DATA_WIDTH - 1 downto 0) is WB_I(DATA_WIDTH + ADDR_WIDTH - 1 downto ADDR_WIDTH);
+	alias adr_ms  : std_logic_vector(ADDR_WIDTH - 1 downto 0) is WB_MS(ADDR_WIDTH - 1 downto 0);
+	alias dat_ms  : std_logic_vector(DATA_WIDTH - 1 downto 0) is WB_MS(DATA_WIDTH + ADDR_WIDTH - 1 downto ADDR_WIDTH);
 	signal dat_sm : std_logic_vector(DATA_WIDTH - 1 downto 0);
-	alias stb_ms  : std_logic is WB_I(ADDR_WIDTH + DATA_WIDTH);
-	alias we_ms   : std_logic is WB_I(ADDR_WIDTH + DATA_WIDTH + 1);
-	alias cyc_ms  : std_logic is WB_I(ADDR_WIDTH + DATA_WIDTH + 2);
+	alias we_ms   : std_logic is WB_MS(ADDR_WIDTH + DATA_WIDTH);
+	alias stb_ms  : std_logic is WB_MS(ADDR_WIDTH + DATA_WIDTH + 1);
+	alias cyc_ms  : std_logic is WB_MS(ADDR_WIDTH + DATA_WIDTH + 2);
 	signal ack_sm : std_logic;
 
 	--Addressing Architecture
@@ -85,7 +86,7 @@ begin
 
 	--Core Address --> equals IP Address(core_addr_width-1:0)
 	core_addr <= unsigned(adr_ms(CORE_ADDR_WIDTH - 1 downto 0));
-	core_sel  <= '1' when (adr_ms(ADDR_WIDTH - 1 downto CORE_ADDR_WIDTH) = std_logic_vector(BASE_ADDR(ADDR_WIDTH - 1 downto CORE_ADDR_WIDTH))) else '0';
+	core_sel  <= '1' when (adr_ms(ADDR_WIDTH - 1 downto CORE_ADDR_WIDTH) = std_logic_vector(BASE_ADDR(ADDR_WIDTH + 1 downto CORE_ADDR_WIDTH + 2))) else '0';
 	user_sel  <= core_sel when core_addr > 3 else '0';
 
 	--Generate Bus registers
@@ -97,7 +98,7 @@ begin
 		--WISHBONE Register
 		reg : wb_register
 			generic map(
-				DATA_WIDTH => DATA_WIDTH,
+				DATA_WIDTH   => DATA_WIDTH,
 				DEFAULT_DATA => x"AAAA7777"
 			)
 			port map(
@@ -121,7 +122,7 @@ begin
 	end generate data_reduction;
 
 	--Generate WB Output Port tri-state buffers for each line
-	WB_O <= ack_sm & dat_sm when core_sel = '1' else (others => '0');
+	WB_SM <= ack_sm & dat_sm when core_sel = '1' else (others => '0');
 
 	--WB Input Ports
 	DAT_I <= dat_ms(CORE_DATA_WIDTH - 1 downto 0);
@@ -131,4 +132,5 @@ begin
 	cyc_I <= cyc_ms and user_sel;
 
 end Behavioral;
+
 
