0f5365c6b64074d1ca785bc1c2c73f17787ce37d
Matthew Bridges <matthewbridges88@gmail.com>
2013-06-19 10:49:57 +0200

    Updates dcores to include changes made to gpio_controller_ip

--- DUGONG_IP_CORE_Lib/dcores.vhd
+++ DUGONG_IP_CORE_Lib/dcores.vhd
@@ -1,18 +1,20 @@
---  
 --                    
--- _____/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\_______      
---\____/\\\///////\\\____\/\\\_______\/\\\___\/////\\\///____\/\\\\\\___\/\\\_______/\\\///\\\_____\
--- \___\/\\\_____\/\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\/\\\__\/\\\_____/\\\/__\///\\\___\    
---  \___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\   
---   \___\/\\\//////\\\_____\/\\\/////////\\\_______\/\\\_______\/\\\\//\\\\/\\\___\/\\\_______\/\\\__\  
---    \___\/\\\____\//\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\_\//\\\/\\\___\//\\\______/\\\___\
---     \___\/\\\_____\//\\\___\/\\\_______\/\\\_______\/\\\_______\/\\\__\//\\\\\\____\///\\\__/\\\_____\
---      \___\/\\\______\//\\\__\/\\\_______\/\\\____/\\\\\\\\\\\___\/\\\___\//\\\\\______\///\\\\\/______\
---       \___\///________\///___\///________\///____\///////////____\///_____\/////_________\/////________\
---        \                                                                                                \
---         \==============  Reconfigurable Hardware Interface for computatioN and radiO  ===================\
---          \============================  http://www.rhinoplatform.org  ====================================\
---           \================================================================================================\
+-- _______/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\_________     
+-- \ ____/\\\///////\\\____\/\\\_______\/\\\___\/////\\\///____\/\\\\\\___\/\\\_______/\\\///\\\_____\
+--  \ ___\/\\\_____\/\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\/\\\__\/\\\_____/\\\/__\///\\\___\    
+--   \ ___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\   
+--    \ ___\/\\\//////\\\_____\/\\\/////////\\\_______\/\\\_______\/\\\\//\\\\/\\\___\/\\\_______\/\\\__\  
+--     \ ___\/\\\____\//\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\_\//\\\/\\\___\//\\\______/\\\___\
+--      \ ___\/\\\_____\//\\\___\/\\\_______\/\\\_______\/\\\_______\/\\\__\//\\\\\\____\///\\\__/\\\_____\
+--       \ ___\/\\\______\//\\\__\/\\\_______\/\\\____/\\\\\\\\\\\___\/\\\___\//\\\\\______\///\\\\\/______\
+--        \ ___\///________\///___\///________\///____\///////////____\///_____\/////_________\/////________\
+--         \ __________________________________________\          \__________________________________________\
+--          |:------------------------------------------|: DUGONG :|-----------------------------------------:|
+--         / ==========================================/          /========================================= /
+--        / =============================================================================================== /
+--       / ================  Reconfigurable Hardware Interface for computatioN and radiO  ================ /
+--      / ===============================  http://www.rhinoplatform.org  ================================ /
+--     / =============================================================================================== /
 --
 ---------------------------------------------------------------------------------------------------------------
 -- Company:		UNIVERSITY OF CAPE TOWN
@@ -22,8 +24,8 @@
 -- Type:		PACKAGE (1)
 -- Description:		A package containing DUGONG IP Cores
 --
--- Compliance:		DUGONG V1.1 (1-1)
--- ID:			x 1-1-1-001
+-- Compliance:		DUGONG V1.3
+-- ID:			x 1-3-1-001
 ---------------------------------------------------------------------------------------------------------------
 
 library IEEE;
@@ -35,37 +37,6 @@ use DUGONG_IP_CORE_Lib.dprimitives.ALL;
 
 package dcores is
 
-	------------------------------
-	---- ARM SIDE INTERFACING ----
-	------------------------------ 
-
-	component gpmc_m is
-		generic(
-			DATA_WIDTH : natural := 32;
-			ADDR_WIDTH : natural := 28
-		);
-		port(
-			--System Control Inputs
-			CLK_I           : in    STD_LOGIC;
-			RST_I           : in    STD_LOGIC;
-			--Wishbone Master Lines
-			ADR_O           : out   STD_LOGIC_VECTOR(ADDR_WIDTH - 1 downto 0);
-			DAT_I           : in    STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
-			DAT_O           : out   STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
-			WE_O            : out   STD_LOGIC;
-			STB_O           : out   STD_LOGIC;
-			ACK_I           : in    STD_LOGIC;
-			--GPMC Interface
-			GPMC_CLK_I      : in    STD_LOGIC;
-			GPMC_D_B        : inout STD_LOGIC_VECTOR(15 downto 0);
-			GPMC_A_I        : in    STD_LOGIC_VECTOR(10 downto 1);
-			GPMC_nCS_I      : in    STD_LOGIC_VECTOR(6 downto 0);
-			GPMC_nADV_ALE_I : in    STD_LOGIC;
-			GPMC_nWE_I      : in    STD_LOGIC;
-			GPMC_nOE_I      : in    STD_LOGIC
-		);
-	end component gpmc_m;
-
 	---------------------------
 	---- WISHBONE IP CORES ----
 	---------------------------
@@ -74,7 +45,7 @@ package dcores is
 		generic(
 			DATA_WIDTH      : NATURAL               := 32;
 			ADDR_WIDTH      : NATURAL               := 12;
-			BASE_ADDR       : UNSIGNED(11 downto 0) := x"000";
+			BASE_ADDR       : UNSIGNED(15 downto 0) := x"0000";
 			CORE_DATA_WIDTH : NATURAL               := 32;
 			CORE_ADDR_WIDTH : NATURAL               := 10
 		);
@@ -133,21 +104,22 @@ package dcores is
 
 	component gpio_controller_ip
 		generic(
-			DATA_WIDTH      : NATURAL               := 32;
-			ADDR_WIDTH      : NATURAL               := 12;
-			BASE_ADDR       : UNSIGNED(11 downto 0) := x"000";
+			BASE_ADDR       : UNSIGNED(15 downto 0) := x"0000";
 			CORE_DATA_WIDTH : NATURAL               := 16;
 			CORE_ADDR_WIDTH : NATURAL               := 3
 		);
 		port(
 			--System Control Inputs
-			CLK_I : in    STD_LOGIC;
-			RST_I : in    STD_LOGIC;
-			--Slave to WHISHBONE
-			WB_I  : in    STD_LOGIC_VECTOR(2 + ADDR_WIDTH + DATA_WIDTH downto 0);
-			WB_O  : out   STD_LOGIC_VECTOR(DATA_WIDTH downto 0);
+			CLK_I         : in    STD_LOGIC;
+			RST_I         : in    STD_LOGIC;
+			--Slave to WB
+			WB_MS         : in    WB_MS_type;
+			WB_SM         : out   WB_SM_type;
+			--GPIO Stream Interface
+			GPIO_STREAM_I : out   STD_LOGIC_VECTOR(CORE_DATA_WIDTH - 1 downto 0);
+			GPIO_STREAM_O : in    STD_LOGIC_VECTOR(CORE_DATA_WIDTH - 1 downto 0);
 			--GPIO Interface
-			GPIO  : inout STD_LOGIC_VECTOR(CORE_DATA_WIDTH - 1 downto 0)
+			GPIO_B        : inout STD_LOGIC_VECTOR(CORE_DATA_WIDTH - 1 downto 0)
 		);
 	end component gpio_controller_ip;
 
