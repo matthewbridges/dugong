28247d2be8094f7833fac4e780dd4fb6668ea098
Matthew Bridges <matthewbridges88@gmail.com>
2013-06-19 12:38:45 +0200

    Updates BRAM and BRAM_IP to conform with Version 1.4

--- DUGONG_IP_CORE_Lib/bram/rtl/bram_ip.vhd
+++ DUGONG_IP_CORE_Lib/bram/rtl/bram_ip.vhd
@@ -1,29 +1,31 @@
----------------------------------------------------------------------------------------------------------------
 --                    
---______/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\________      
---\____/\\\///////\\\____\/\\\_______\/\\\___\/////\\\///____\/\\\\\\___\/\\\_______/\\\///\\\_____\
--- \___\/\\\_____\/\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\/\\\__\/\\\_____/\\\/__\///\\\___\    
---  \___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\   
---   \___\/\\\//////\\\_____\/\\\/////////\\\_______\/\\\_______\/\\\\//\\\\/\\\___\/\\\_______\/\\\__\  
---    \___\/\\\____\//\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\_\//\\\/\\\___\//\\\______/\\\___\
---     \___\/\\\_____\//\\\___\/\\\_______\/\\\_______\/\\\_______\/\\\__\//\\\\\\____\///\\\__/\\\_____\
---      \___\/\\\______\//\\\__\/\\\_______\/\\\____/\\\\\\\\\\\___\/\\\___\//\\\\\______\///\\\\\/______\
---       \___\///________\///___\///________\///____\///////////____\///_____\/////_________\/////________\
---        \                                                                                                \
---         \==============  Reconfigurable Hardware Interface for computatioN and radiO  ===================\
---          \============================  http://www.rhinoplatform.org  ====================================\
---           \================================================================================================\
+-- _______/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\_________     
+-- \ ____/\\\///////\\\____\/\\\_______\/\\\___\/////\\\///____\/\\\\\\___\/\\\_______/\\\///\\\_____\
+--  \ ___\/\\\_____\/\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\/\\\__\/\\\_____/\\\/__\///\\\___\    
+--   \ ___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\   
+--    \ ___\/\\\//////\\\_____\/\\\/////////\\\_______\/\\\_______\/\\\\//\\\\/\\\___\/\\\_______\/\\\__\  
+--     \ ___\/\\\____\//\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\_\//\\\/\\\___\//\\\______/\\\___\
+--      \ ___\/\\\_____\//\\\___\/\\\_______\/\\\_______\/\\\_______\/\\\__\//\\\\\\____\///\\\__/\\\_____\
+--       \ ___\/\\\______\//\\\__\/\\\_______\/\\\____/\\\\\\\\\\\___\/\\\___\//\\\\\______\///\\\\\/______\
+--        \ ___\///________\///___\///________\///____\///////////____\///_____\/////_________\/////________\
+--         \ __________________________________________\          \__________________________________________\
+--          |:------------------------------------------|: DUGONG :|-----------------------------------------:|
+--         / ==========================================/          /========================================= /
+--        / =============================================================================================== /
+--       / ================  Reconfigurable Hardware Interface for computatioN and radiO  ================ /
+--      / ===============================  http://www.rhinoplatform.org  ================================ /
+--     / =============================================================================================== /
 --
 ---------------------------------------------------------------------------------------------------------------
 -- Company:		UNIVERSITY OF CAPE TOWN
--- Engineer: 		MATTHEW BRIDGES
+-- Engineer:		MATTHEW BRIDGES
 --
 -- Name:		BRAM_IP (001)
 -- Type:		IP CORE (4)
 -- Description: 	An IP core containing BRAM/s which can be used for temporary storage of general data	
 --
--- Compliance:		DUGONG V1.1
--- ID:			x 1-1-4-001
+-- Compliance:		DUGONG V1.4
+-- ID:			x 1-4-4-001
 ---------------------------------------------------------------------------------------------------------------
 
 library IEEE;
@@ -33,54 +35,55 @@ use IEEE.NUMERIC_STD.ALL;
 library DUGONG_IP_CORE_Lib;
 use DUGONG_IP_CORE_Lib.dprimitives.ALL;
 
+--NB The DATA_WIDTH and ADDR_WIDTH constants are set in the dprimitives package
 entity bram_ip is
 	generic(
-		DATA_WIDTH      : NATURAL               := 32;
-		ADDR_WIDTH      : NATURAL               := 12;
-		BASE_ADDR       : UNSIGNED(11 downto 0) := x"000";
+		BASE_ADDR       : UNSIGNED(15 downto 0) := x"0000";
 		CORE_DATA_WIDTH : NATURAL               := 32;
 		CORE_ADDR_WIDTH : NATURAL               := 10
 	);
 	port(
 		--System Control Inputs
-		CLK_I : in  STD_LOGIC;
-		RST_I : in  STD_LOGIC;
+		CLK_I         : in    STD_LOGIC;
+		RST_I         : in    STD_LOGIC;
 		--Slave to WB
-		WB_I  : in  STD_LOGIC_VECTOR(2 + ADDR_WIDTH + DATA_WIDTH downto 0);
-		WB_O  : out STD_LOGIC_VECTOR(DATA_WIDTH downto 0)
+		WB_MS         : in    WB_MS_type;
+		WB_SM         : out   WB_SM_type
 	);
 end bram_ip;
 
 architecture Behavioral of bram_ip is
+	signal adr_i : STD_LOGIC_VECTOR(CORE_ADDR_WIDTH - 1 downto 0);
 	signal dat_i : STD_LOGIC_VECTOR(CORE_DATA_WIDTH - 1 downto 0);
 	signal dat_o : STD_LOGIC_VECTOR(CORE_DATA_WIDTH - 1 downto 0);
-	signal adr_i : STD_LOGIC_VECTOR(CORE_ADDR_WIDTH - 1 downto 0);
-	signal stb_i : STD_LOGIC;
 	signal we_i  : STD_LOGIC;
+	signal stb_i : STD_LOGIC;
 	signal ack_o : STD_LOGIC;
+	signal cyc_i : STD_LOGIC;
 
 	component bram
-		generic(
-			DATA_WIDTH : natural := 32;
-			ADDR_WIDTH : natural := 10
-		);
-		port(
-			CLK_I : in  STD_LOGIC;
-			RST_I : in  STD_LOGIC;
-			DAT_I : in  STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
-			DAT_O : out STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
-			ADR_I : in  STD_LOGIC_VECTOR(ADDR_WIDTH - 1 downto 0);
-			STB_I : in  STD_LOGIC;
-			WE_I  : in  STD_LOGIC;
-			ACK_O : out STD_LOGIC
-		);
+	generic(
+		DATA_WIDTH : natural := 32;
+		ADDR_WIDTH : natural := 10
+	);
+	port(
+		--System Control Inputs
+		CLK_I : in  STD_LOGIC;
+		RST_I : in  STD_LOGIC;
+		--Wishbone Slave Lines
+		ADR_I : in  STD_LOGIC_VECTOR(ADDR_WIDTH - 1 downto 0);
+		DAT_I : in  STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
+		DAT_O : out STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
+		WE_I  : in  STD_LOGIC;
+		STB_I : in  STD_LOGIC;
+		ACK_O : out STD_LOGIC;
+		CYC_I : in  STD_LOGIC
+	);
 	end component bram;
 
 begin
 	bus_logic : wb_s
 		generic map(
-			DATA_WIDTH      => DATA_WIDTH,
-			ADDR_WIDTH      => ADDR_WIDTH,
 			BASE_ADDR       => BASE_ADDR,
 			CORE_DATA_WIDTH => CORE_DATA_WIDTH,
 			CORE_ADDR_WIDTH => CORE_ADDR_WIDTH
@@ -88,15 +91,15 @@ begin
 		port map(
 			CLK_I => CLK_I,
 			RST_I => RST_I,
-			WB_I  => WB_I,
-			WB_O  => WB_O,
+			WB_MS => WB_MS,
+			WB_SM => WB_SM,
+			ADR_I => adr_i,
 			DAT_I => dat_i,
 			DAT_O => dat_o,
-			ADR_I => adr_i,
-			STB_I => stb_i,
 			WE_I  => we_i,
-			CYC_I => open,
-			ACK_O => ack_o
+			STB_I => stb_i,
+			ACK_O => ack_o,
+			CYC_I => cyc_i
 		);
 
 	user_logic : bram
@@ -107,12 +110,13 @@ begin
 		port map(
 			CLK_I => CLK_I,
 			RST_I => RST_I,
+			ADR_I => adr_i,
 			DAT_I => dat_i,
 			DAT_O => dat_o,
-			ADR_I => adr_i,
-			STB_I => stb_i,
 			WE_I  => we_i,
-			ACK_O => ack_o
+			STB_I => stb_i,
+			ACK_O => ack_o,
+			CYC_I => cyc_i
 		);
 
 end Behavioral;
