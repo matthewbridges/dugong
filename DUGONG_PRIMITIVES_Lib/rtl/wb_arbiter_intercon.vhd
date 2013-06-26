10b8d90f3388d72f1bd89947f58f8b89acde4bf0
Matthew Bridges <matthewbridges88@gmail.com>
2013-06-26 11:56:09 +0200

    Improves design of wb_arbiter_intercon

--- DUGONG_PRIMITIVES_Lib/rtl/wb_arbiter_intercon.vhd
+++ DUGONG_PRIMITIVES_Lib/rtl/wb_arbiter_intercon.vhd
@@ -57,9 +57,10 @@ end wb_arbiter_intercon;
 
 architecture Behavioral of wb_arbiter_intercon is
 	signal wb_cyc : std_logic_vector(NUMBER_OF_MASTERS - 1 downto 0);
-	subtype master_num is natural range 0 to NUMBER_OF_MASTERS;
-	signal master_sel : master_num;
-	signal bus_busy   : std_logic;
+	subtype master_num is natural range 0 to NUMBER_OF_MASTERS - 1;
+	signal master_sel : master_num := 0;
+
+	signal bus_busy : std_logic;
 
 begin
 	--Generate wb_cyc registers
@@ -68,23 +69,19 @@ begin
 		wb_cyc(i) <= WB_MS(i)(2 + ADDR_WIDTH + DATA_WIDTH);
 	end generate wb_cyc_registers;
 
+	--Multiplexer
 	process(CLK_I, RST_I)
 	begin
 		--RST STATE
 		if (RST_I = '1') then
-			WB_GNT_O   <= (others => '0');
+			bus_busy <= '0';
 			master_sel <= 0;
-			bus_busy   <= '0';
+			WB_GNT_O <= "00";
 		else
 			--Perform Rising Edge operations
-			if (rising_edge(CLK_I)) then
-				if (bus_busy = '1') then
-					if (master_sel /= NUMBER_OF_MASTERS) then
-						bus_busy <= wb_cyc(master_sel);
-					else
-						bus_busy <= '0';
-					end if;
-				else
+			if (falling_edge(CLK_I)) then
+				if (bus_busy = '0') then
+					bus_busy <= wb_cyc(0) or wb_cyc(1);
 					case (wb_cyc) is
 						when "01" => master_sel <= 0;
 							WB_GNT_O          <= "01";
@@ -92,16 +89,18 @@ begin
 							WB_GNT_O          <= "10";
 						when "11" => master_sel   <= 0;
 							WB_GNT_O          <= "01";
-						when others => master_sel <= NUMBER_OF_MASTERS;
+						when others => master_sel <= 0;
 							WB_GNT_O          <= "00";
 					end case;
-					bus_busy <= wb_cyc(0) or wb_cyc(1);
+				elsif (wb_cyc(master_sel) = '0') then
+					bus_busy <= '0';
+					WB_GNT_O <= "00";
 				end if;
 			end if;
 		end if;
 	end process;
 
-	WB_MS_BUS <= WB_MS(master_sel) when (master_sel /= NUMBER_OF_MASTERS) else (others => '0');
+	WB_MS_BUS <= WB_MS(master_sel) when (bus_busy = '1') else (others => '0');
 
 	WB_SM_BUS <= WB_SM(0) or WB_SM(1) or WB_SM(2) or WB_SM(3);
 end Behavioral;
