c4e130078966e87f5b99f61115b5d137e3e08ddd
Matthew Bridges <matthewbridges88@gmail.com>
2013-06-26 14:19:18 +0200

    Adds wb_latch element for use in peripherals

---
+++ DUGONG_PRIMITIVES_Lib/rtl/wb_latch.vhd
@@ -0,0 +1,70 @@
+--  
+--                    
+-- _____/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\_______      
+--\____/\\\///////\\\____\/\\\_______\/\\\___\/////\\\///____\/\\\\\\___\/\\\_______/\\\///\\\_____\
+-- \___\/\\\_____\/\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\/\\\__\/\\\_____/\\\/__\///\\\___\    
+--  \___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\   
+--   \___\/\\\//////\\\_____\/\\\/////////\\\_______\/\\\_______\/\\\\//\\\\/\\\___\/\\\_______\/\\\__\  
+--    \___\/\\\____\//\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\_\//\\\/\\\___\//\\\______/\\\___\
+--     \___\/\\\_____\//\\\___\/\\\_______\/\\\_______\/\\\_______\/\\\__\//\\\\\\____\///\\\__/\\\_____\
+--      \___\/\\\______\//\\\__\/\\\_______\/\\\____/\\\\\\\\\\\___\/\\\___\//\\\\\______\///\\\\\/______\
+--       \___\///________\///___\///________\///____\///////////____\///_____\/////_________\/////________\
+--        \                                                                                                \
+--         \==============  Reconfigurable Hardware Interface for computatioN and radiO  ===================\
+--          \============================  http://www.rhinoplatform.org  ====================================\
+--           \================================================================================================\
+--
+---------------------------------------------------------------------------------------------------------------
+-- Company:		UNIVERSITY OF CAPE TOWN
+-- Engineer:		MATTHEW BRIDGES
+--
+-- Name:		WB_LATCH
+-- Type:		PRIMITIVE (2)
+-- Description:		A latch primitive with one port which can take on generic data widths and default values.
+--
+-- Compliance:		DUGONG V1.1
+-- ID:			x 1-1-2-
+---------------------------------------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity wb_latch is
+	generic(
+		DATA_WIDTH   : NATURAL                       := 32;
+		DEFAULT_DATA : STD_LOGIC_VECTOR(31 downto 0) := x"00000000"
+	);
+	port(
+		--System Control Inputs:
+		CLK_I : in  STD_LOGIC;
+		RST_I : in  STD_LOGIC;
+		--WISHBONE SLAVE interface:1-2
+		DAT_I : in  STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
+		DAT_O : out STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
+		STB_I : in  STD_LOGIC;
+		ACK_O : out STD_LOGIC
+	);
+end wb_latch;
+
+architecture Behavioral of wb_latch is
+	signal Q : std_logic_vector(DATA_WIDTH - 1 downto 0);
+
+begin
+	process(CLK_I, RST_I)
+	begin
+		--RST STATE
+		if (RST_I = '1') then
+			Q <= DEFAULT_DATA(DATA_WIDTH - 1 downto 0);
+		else
+			--Perform Clock Rising Edge operations
+			if (rising_edge(CLK_I)) then
+				--WRITING STATE
+				Q <= DAT_I;
+			end if;
+		end if;
+	end process;
+	ACK_O <= STB_I;
+	DAT_O <= Q;
+
+end Behavioral;
