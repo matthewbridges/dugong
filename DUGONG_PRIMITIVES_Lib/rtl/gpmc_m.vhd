6c11a52c22adb7e6b9ab84dc35bf09142eae05d4
Matthew Bridges <matthewbridges88@gmail.com>
2013-07-01 11:15:37 +0200

    Adds seperate read and write strobe/ack signals and sets the error output to be the address in place of the signal 0xEEEEEEEE

--- DUGONG_PRIMITIVES_Lib/rtl/gpmc_m.vhd
+++ DUGONG_PRIMITIVES_Lib/rtl/gpmc_m.vhd
@@ -1,5 +1,5 @@
 --                    
--- _______/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\_________     
+-- _______/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\________    
 -- \ ____/\\\///////\\\____\/\\\_______\/\\\___\/////\\\///____\/\\\\\\___\/\\\_______/\\\///\\\_____\
 --  \ ___\/\\\_____\/\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\/\\\__\/\\\_____/\\\/__\///\\\___\    
 --   \ ___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\   
@@ -79,62 +79,77 @@ architecture Behavioral of gpmc_m is
 
 	signal gpmc_dout : std_logic_vector(15 downto 0);
 
-	signal word_sel : std_logic;
-	signal gpmc_stb : std_logic;
-	signal gpmc_ack : std_logic;
+	signal word_sel            : std_logic;
+	signal read_stb            : std_logic;
+	signal read_ack            : std_logic;
+	signal write_stb           : std_logic;
+	signal write_ack           : std_logic;
+	signal gpmc_data_not_valid : std_logic;
 
 begin
-	process(CLK_I, RST_I, gpmc_stb, gpmc_ack)
+	process(CLK_I, RST_I, ACK_I, cyc_ms, read_stb, read_ack, write_stb, write_ack)
 	begin
 		--RST STATE
 		if (RST_I = '1') then
-			dat_sm   <= (others => '0');
-			we_ms    <= '0';
-			stb_ms   <= '0';
-			cyc_ms   <= '0';
-			gpmc_ack <= '0';
+			dat_sm    <= (others => '0');
+			we_ms     <= '0';
+			stb_ms    <= '0';
+			cyc_ms    <= '0';
+			read_ack  <= '0';
+			write_ack <= '0';
 		else
-			if ((gpmc_stb = '0') and (gpmc_ack = '1')) then
-				gpmc_ack <= '0';
-			else
-				--Perform Clock Rising Edge operations
-				if (rising_edge(CLK_I)) then
-					if (stb_ms = '1') then
-						if (ACK_I = '1') then
-							dat_sm   <= DAT_I;
-							we_ms    <= '0';
-							stb_ms   <= '0';
-							cyc_ms   <= '0';
-							gpmc_ack <= '1';
-						elsif (ERR_I = '1') then
-							dat_sm   <= x"EEEEEEEE";
-							we_ms    <= '0';
-							stb_ms   <= '0';
-							cyc_ms   <= '0';
-							gpmc_ack <= '1';
-						end if;
-					elsif (gpmc_stb = '1') then
-						stb_ms <= '1';
-						cyc_ms <= '1';
-						we_ms  <= word_sel;
+			--Perform Clock Rising Edge operations
+			if (rising_edge(CLK_I)) then
+				if (stb_ms = '1') then
+					if (ACK_I = '1') then
+						dat_sm    <= DAT_I;
+						we_ms     <= '0';
+						stb_ms    <= '0';
+						read_ack  <= not we_ms;
+						write_ack <= we_ms;
+					elsif (ERR_I = '1') then
+						dat_sm    <= "00" & adr_ms & "00";
+						we_ms     <= '0';
+						stb_ms    <= '0';
+						read_ack  <= not we_ms;
+						write_ack <= we_ms;
 					end if;
+				elsif ((read_stb or write_stb) = '1') then
+					if ((read_ack and write_ack) = '0') then
+						cyc_ms <= '1';
+						stb_ms <= '1';
+						we_ms  <= write_stb;
+					end if;
+				else
+					cyc_ms <= '0';
+				end if;
+
+				if (read_ack = '1') then
+					read_ack <= read_stb;
+				end if;
+
+				if (write_ack = '1') then
+					write_ack <= write_stb;
 				end if;
 			end if;
 		end if;
-
 	end process;
 
-	process(GPMC_CLK_I, RST_I, gpmc_stb, gpmc_ack)
+	process(GPMC_CLK_I, RST_I, read_stb, read_ack, write_stb, write_ack)
 	begin
 		--RST STATE
 		if (RST_I = '1') then
-			gpmc_stb <= '0';
-			word_sel <= '0';
-			adr_ms   <= (others => '0');
-			dat_ms   <= (others => '0');
+			read_stb            <= '0';
+			write_stb           <= '0';
+			gpmc_data_not_valid <= '0';
+			word_sel            <= '0';
+			adr_ms              <= (others => '0');
+			dat_ms              <= (others => '0');
 		else
-			if ((gpmc_stb = '1') and (gpmc_ack = '1')) then
-				gpmc_stb <= '0';
+			if ((read_stb = '1') and (read_ack = '1')) then
+				read_stb <= '0';
+			elsif ((write_stb = '1') and (write_ack = '1')) then
+				write_stb <= '0';
 			else
 				--Perform Clock Rising Edge operations
 				if (rising_edge(GPMC_CLK_I)) then
@@ -153,14 +168,18 @@ begin
 								when "0111111" => adr_ms(GPMC_ADDR_WIDTH - 1 downto GPMC_ADDR_WIDTH - 3) <= "111"; --0x38000000
 								when others    => adr_ms(GPMC_ADDR_WIDTH - 1 downto GPMC_ADDR_WIDTH - 3) <= "110"; --0x30000000
 							end case;
-							gpmc_stb <= not GPMC_D_B(0);
+							read_stb            <= not GPMC_D_B(0); --Read on first cycle
+							gpmc_data_not_valid <= '1';
 						elsif (GPMC_nWE_I = '0') then
 							if (word_sel = '1') then
 								dat_ms(31 downto 16) <= GPMC_D_B;
-								gpmc_stb             <= '1';
+								write_stb            <= '1'; --Write on second cycle
 							else
 								dat_ms(15 downto 0) <= GPMC_D_B;
 							end if;
+							gpmc_data_not_valid <= '0';
+						elsif (GPMC_nOE_I = '0') then
+							gpmc_data_not_valid <= read_stb;
 						end if;
 					end if;
 				end if;
@@ -174,16 +193,13 @@ begin
 	STB_O <= stb_ms;
 	CYC_O <= cyc_ms;
 
-	--gpmc_dout <= x"0" & adr_ms(GPMC_ADDR_WIDTH - 1 downto 16) when word_sel = '1' else adr_ms(15 downto 0);
 	gpmc_dout <= dat_sm(31 downto 16) when word_sel = '1' else dat_sm(15 downto 0);
 
-	GPMC_WAIT_O <= not gpmc_stb;
+	GPMC_WAIT_O <= not gpmc_data_not_valid;
 
 	--GPMC tri-state buffers for GPMC Bidirectional Data Bus
 	GPMC_D_B <= gpmc_dout when GPMC_nOE_I = '0' else (others => 'Z');
-	--GPMC_D_B <= x"FEDC" when GPMC_nOE_I = '0' else (others => 'Z');
 
-	DEBUG <= adr_ms(4 downto 0) & we_ms & stb_ms & ACK_I & cyc_ms & ERR_I & GPMC_CLK_I & GPMC_A_I & GPMC_nCS_I & GPMC_nADV_ALE_I & GPMC_nWE_I & GPMC_nOE_I & gpmc_stb;
+	DEBUG <= word_sel & read_stb & read_ack & write_stb & write_ack & we_ms & stb_ms & ACK_I & cyc_ms & ERR_I & GPMC_CLK_I & GPMC_A_I & GPMC_nCS_I & GPMC_nADV_ALE_I & GPMC_nWE_I & GPMC_nOE_I & not gpmc_data_not_valid;
 
 end Behavioral;
-	
