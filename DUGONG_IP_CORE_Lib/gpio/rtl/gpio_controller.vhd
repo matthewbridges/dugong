8980285b4eaa902776e519e1029670ce3ca00a3f
Matthew Bridges <matthewbridges88@gmail.com>
2013-07-01 11:21:08 +0200

    Applies many changes, all of which are to account for the new wb_latch primitive, also allows user defined addressing based on the constant user_addr

--- DUGONG_IP_CORE_Lib/gpio/rtl/gpio_controller.vhd
+++ DUGONG_IP_CORE_Lib/gpio/rtl/gpio_controller.vhd
@@ -1,8 +1,8 @@
 --                    
--- _______/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\_________     
+-- _______/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\________
 -- \ ____/\\\///////\\\____\/\\\_______\/\\\___\/////\\\///____\/\\\\\\___\/\\\_______/\\\///\\\_____\
 --  \ ___\/\\\_____\/\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\/\\\__\/\\\_____/\\\/__\///\\\___\    
---   \ ___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\   
+--   \ ___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\
 --    \ ___\/\\\//////\\\_____\/\\\/////////\\\_______\/\\\_______\/\\\\//\\\\/\\\___\/\\\_______\/\\\__\  
 --     \ ___\/\\\____\//\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\_\//\\\/\\\___\//\\\______/\\\___\
 --      \ ___\/\\\_____\//\\\___\/\\\_______\/\\\_______\/\\\_______\/\\\__\//\\\\\\____\///\\\__/\\\_____\
@@ -28,12 +28,14 @@
 -- Compliance:		DUGONG V1.3
 -- ID:			x 1-3-3-002
 ---------------------------------------------------------------------------------------------------------------
--- ADDR	-> REG_NAME
--- 0 	-> GPIO_OUT_REG
--- 1	-> N/A
--- 2	-> DIR
--- 3	-> STREAM
----------------------------------------------------------------------------------------------------------------
+--	ADDR	| NAME		| Type		--
+--	0	| GPIO_OUT	| WB_REG	--
+-- 	1	| GPIO_IN	| WB_LATCH	--
+-- 	2	| Output_Enable	| WB_REG	--
+-- 	3	| AUX_Enable	| WB_REG	--
+--------------------------------------------------
+
+--  ( http://opencores.org/project,gpio ) was used for the design of this core
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -44,26 +46,27 @@ use DUGONG_PRIMITIVES_Lib.dprimitives.ALL;
 
 entity gpio_controller is
 	generic(
-		DATA_WIDTH : natural := 16;
-		ADDR_WIDTH : natural := 3
+		DATA_WIDTH          : natural := 16;
+		ADDR_WIDTH          : natural := 3;
+		NUMBER_OF_REGISTERS : natural := 3
 	);
 	port(
 		--System Control Inputs
-		CLK_I         : in    STD_LOGIC;
-		RST_I         : in    STD_LOGIC;
+		CLK_I      : in    STD_LOGIC;
+		RST_I      : in    STD_LOGIC;
 		--Wishbone Slave Lines
-		ADR_I         : in    STD_LOGIC_VECTOR(ADDR_WIDTH - 1 downto 0);
-		DAT_I         : in    STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
-		DAT_O         : out   STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
-		WE_I          : in    STD_LOGIC;
-		STB_I         : in    STD_LOGIC;
-		ACK_O         : out   STD_LOGIC;
-		--CYC_I : in   STD_LOGIC;
-		--GPIO Stream Interface
-		GPIO_STREAM_O : out   STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
-		GPIO_STREAM_I : in    STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
+		ADR_I      : in    STD_LOGIC_VECTOR(ADDR_WIDTH - 1 downto 0);
+		DAT_I      : in    STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
+		DAT_O      : out   STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
+		WE_I       : in    STD_LOGIC;
+		STB_I      : in    STD_LOGIC;
+		ACK_O      : out   STD_LOGIC;
+		CYC_I      : in    STD_LOGIC;
+		--GPIO Auxiliary Interface
+		GPIO_AUX_O : out   STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
+		GPIO_AUX_I : in    STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
 		--GPIO Interface
-		GPIO_B        : inout STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0)
+		GPIO_B     : inout STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0)
 	);
 end gpio_controller;
 
@@ -71,50 +74,91 @@ architecture Behavioral of gpio_controller is
 
 	--User memory architecture
 	type ram_type is array (0 to (2 ** ADDR_WIDTH) - 5) of std_logic_vector(DATA_WIDTH - 1 downto 0);
-	signal user_mem  : ram_type                                     := (others => (others => '0'));
-	signal user_addr : integer                                      := 0;
-	signal stb       : std_logic_vector(0 to (2 ** ADDR_WIDTH) - 5) := (others => '0');
-	signal ack       : std_logic_vector(0 to (2 ** ADDR_WIDTH) - 5) := (others => '0');
+	signal user_mem : ram_type                                           := (others => (others => '0'));
+	signal user_stb : std_logic_vector(((2 ** ADDR_WIDTH) - 5) downto 0) := (others => '0');
+	signal user_ack : std_logic_vector(((2 ** ADDR_WIDTH) - 5) downto 0) := (others => '0');
+
+	subtype small_int is integer range 0 to 2 ** ADDR_WIDTH - 5;
+	type t is array (0 to (2 ** ADDR_WIDTH) - 5) of small_int;
+	constant user_addr : t := (0, 2, 3, 1); --Addresses of registers followed by Addresses of Latches
+	--------------------------------------------------
+	--	ADDR	| NAME		| Type		--
+	--	0	| GPIO_OUT	| WB_REG	--
+	-- 	1	| GPIO_IN	| WB_LATCH	--
+	-- 	2	| Output_Enable	| WB_REG	--
+	-- 	3	| AUX_Enable	| WB_REG	--
+	--------------------------------------------------
+
+	signal wb_addr : small_int;
 
 	--GPIO internal signal
-	signal gpio : std_logic_vector(DATA_WIDTH - 1 downto 0);
+	signal gpio_o : std_logic_vector(DATA_WIDTH - 1 downto 0);
+	signal gpio_i : std_logic_vector(DATA_WIDTH - 1 downto 0);
 
 begin
 
 	--User Memory Address --> equals IP Address(core_addr_width-1:0) - 4
-	user_addr <= to_integer(unsigned(ADR_I));
+	wb_addr <= to_integer(unsigned(ADR_I));
 
-	--Generate GPIO registers
-	user_registers : for addr in 0 to (2 ** ADDR_WIDTH) - 5 generate
+	--Generate WB registers
+	user_registers : for i in 0 to (NUMBER_OF_REGISTERS - 1) generate
 	begin
-		--Check for valid addr
-		stb(addr) <= STB_I when user_addr = addr else '0';
-
 		--WISHBONE Register
 		reg : wb_register
 			generic map(
-				DATA_WIDTH => DATA_WIDTH
+				DATA_WIDTH   => DATA_WIDTH,
+				DEFAULT_DATA => x"00000000"
 			)
 			port map(
 				CLK_I => CLK_I,
 				RST_I => RST_I,
 				DAT_I => DAT_I,
-				DAT_O => user_mem(addr),
+				DAT_O => user_mem(user_addr(i)),
 				WE_I  => WE_I,
-				STB_I => stb(addr),
-				ACK_O => ack(addr)
+				STB_I => user_stb(user_addr(i)),
+				ACK_O => user_ack(user_addr(i))
 			);
-
 	end generate user_registers;
 
-	DAT_O <= user_mem(user_addr);
-	ACK_O <= ack(user_addr);
+	--Generate WB Latches
+	user_latches : for i in NUMBER_OF_REGISTERS to ((2 ** ADDR_WIDTH) - 5) generate
+	begin
+		--WISHBONE Latches
+		latch : wb_latch
+			generic map(
+				DATA_WIDTH   => DATA_WIDTH,
+				DEFAULT_DATA => x"00000000"
+			)
+			port map(
+				CLK_I => CLK_I,
+				RST_I => RST_I,
+				DAT_I => gpio_i,
+				DAT_O => user_mem(user_addr(i)),
+				STB_I => user_stb(user_addr(i)),
+				ACK_O => user_ack(user_addr(i))
+			);
+	end generate user_latches;
+
+	--Generate user Strobe lines
+	user_registers_control : for i in 0 to ((2 ** ADDR_WIDTH) - 5) generate
+	begin
+		--Check for valid addr
+		user_stb(i) <= (STB_I and CYC_I) when (wb_addr = i) else '0';
+	end generate user_registers_control;
+
+	DAT_O <= user_mem(wb_addr);
+	ACK_O <= user_ack(wb_addr);
 
 	--Generate GPIO tri-state buffers and multiplexors for each GPIO pin
 	gpio_control_buffers : for gpio_num in 0 to DATA_WIDTH - 1 generate
-		gpio(gpio_num)          <= GPIO_STREAM_I(gpio_num) when user_mem(3)(gpio_num) = '1' else user_mem(0)(gpio_num); -- STREAM REG Control
-		GPIO_B(gpio_num)        <= gpio(gpio_num) when user_mem(2)(gpio_num) = '1' else 'Z'; -- DIRECTION Control
-		GPIO_STREAM_O(gpio_num) <= GPIO_B(gpio_num) when user_mem(2)(gpio_num) = '0' else '0';
+		--Multiplexer for Auxiliary input
+		gpio_o(gpio_num)     <= GPIO_AUX_I(gpio_num) when user_mem(3)(gpio_num) = '1' else user_mem(0)(gpio_num);
+		--Auxiliary output  only if Output Enable is true
+		GPIO_AUX_O(gpio_num) <= '0' when user_mem(2)(gpio_num) = '1' else gpio_i(gpio_num);
+
+		--Tri-state Buffer
+		GPIO_B(gpio_num) <= gpio_o(gpio_num) when user_mem(2)(gpio_num) = '1' else 'Z';
+		gpio_i(gpio_num) <= GPIO_B(gpio_num);
 	end generate gpio_control_buffers;
 
 end Behavioral;
