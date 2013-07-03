f3dd199334537d1fe4f704b594de3304aa587c59
Matthew Bridges <matthewbridges88@gmail.com>
2013-07-03 11:32:17 +0200

    Cleans up Header of wb_register

--- DUGONG_PRIMITIVES_Lib/rtl/wb_register.vhd
+++ DUGONG_PRIMITIVES_Lib/rtl/wb_register.vhd
@@ -1,9 +1,9 @@
---                    
--- _______/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\________   
+--
+-- _______/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\________    
 -- \ ____/\\\///////\\\____\/\\\_______\/\\\___\/////\\\///____\/\\\\\\___\/\\\_______/\\\///\\\_____\
---  \ ___\/\\\_____\/\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\/\\\__\/\\\_____/\\\/__\///\\\___\    
---   \ ___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\   
---    \ ___\/\\\//////\\\_____\/\\\/////////\\\_______\/\\\_______\/\\\\//\\\\/\\\___\/\\\_______\/\\\__\  
+--  \ ___\/\\\_____\/\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\/\\\__\/\\\_____/\\\/__\///\\\___\
+--   \ ___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\
+--    \ ___\/\\\//////\\\_____\/\\\/////////\\\_______\/\\\_______\/\\\\//\\\\/\\\___\/\\\_______\/\\\__\
 --     \ ___\/\\\____\//\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\_\//\\\/\\\___\//\\\______/\\\___\
 --      \ ___\/\\\_____\//\\\___\/\\\_______\/\\\_______\/\\\_______\/\\\__\//\\\\\\____\///\\\__/\\\_____\
 --       \ ___\/\\\______\//\\\__\/\\\_______\/\\\____/\\\\\\\\\\\___\/\\\___\//\\\\\______\///\\\\\/______\
@@ -18,14 +18,14 @@
 --
 ---------------------------------------------------------------------------------------------------------------
 -- Company:		UNIVERSITY OF CAPE TOWN
--- Engineer:		MATTHEW BRIDGES
+-- Engineer: 		MATTHEW BRIDGES
 --
 -- Name:		WB_REGISTER (001)
 -- Type:		PRIMITIVE (2)
 -- Description:		A register primitive with one port which can take on generic data widths and default values.
 --
--- Compliance:		DUGONG V1.1 (1-1)
--- ID:			x 1-1-2-001
+-- Compliance:		DUGONG V0.3
+-- ID:			x 0-3-2-001
 ---------------------------------------------------------------------------------------------------------------
 
 library IEEE;
@@ -45,43 +45,39 @@ entity wb_register is
 		DAT_I : in  STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
 		DAT_O : out STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
 		WE_I  : in  STD_LOGIC;
-		--SEL_I : in  STD_LOGIC_VECTOR(DATA_WIDTH / 8 - 1 downto 0);
 		STB_I : in  STD_LOGIC;
 		ACK_O : out STD_LOGIC
-	--CYC_I : in   STD_LOGIC;
 	);
 end wb_register;
 
 architecture Behavioral of wb_register is
-	signal Q : std_logic_vector(DATA_WIDTH - 1 downto 0);
+	signal Q   : std_logic_vector(DATA_WIDTH - 1 downto 0);
+	signal ack : std_logic;
 
 begin
 	process(CLK_I, RST_I, STB_I)
 	begin
 		--RST STATE
 		if (RST_I = '1') then
-			Q     <= DEFAULT_DATA(DATA_WIDTH - 1 downto 0);
-			ACK_O <= '0';
+			Q   <= DEFAULT_DATA(DATA_WIDTH - 1 downto 0);
+			ack <= '0';
 		else
 			if (STB_I = '0') then
-				ACK_O <= '0';
+				ack <= '0';
 			else
 				--Perform Clock Rising Edge operations
 				if (rising_edge(CLK_I)) then
-
 					--WRITING STATE
 					if ((STB_I and WE_I) = '1') then
 						Q <= DAT_I;
-					--IDLE or READING STATE
-					else
-						Q <= Q;
 					end if;
-					ACK_O <= STB_I;
+					ack <= STB_I;
 				end if;
 			end if;
 		end if;
 	end process;
 
 	DAT_O <= Q;
+	ACK_O <= ack;
 
 end Behavioral;
