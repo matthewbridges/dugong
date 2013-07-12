e9cfb3a70f853de7350c27313ee18b9ede0e494d
Matthew Bridges <matthewbridges88@gmail.com>
2013-07-12 16:32:06 +0200

    Adds code to perform address offset on the core itself

--- DUGONG_IP_CORE_Lib/gpio/rtl/gpio_controller.vhd
+++ DUGONG_IP_CORE_Lib/gpio/rtl/gpio_controller.vhd
@@ -109,8 +109,18 @@ begin
 	----------{ BUS LOGIC }----------
 	---------------------------------
 
-	--User Memory Address --> equals IP Address(core_addr_width-1:0) - 4
-	wb_addr <= to_integer(unsigned(ADR_I));
+	--User Memory Address space is equals from 4 up to IP Address(core_addr_width-1:0)
+	addr_generate : if (CORE_ADDR_WIDTH /= 3) generate
+	begin
+		-- Account for offset of 4 due to wb_s status registers
+		wb_addr <= to_integer(unsigned(ADR_I) - 4);
+	end generate addr_generate;
+
+	addr_generate_2 : if (CORE_ADDR_WIDTH = 3) generate
+	begin
+		-- Account for offset of 4 due to wb_s status registers
+		wb_addr <= to_integer(unsigned(ADR_I(CORE_ADDR_WIDTH - 2 downto 0))) when (ADR_I(CORE_ADDR_WIDTH - 1) = '1') else 0;
+	end generate addr_generate_2;
 
 	--Generate WB registers
 	user_registers : if (NUMBER_OF_REGISTERS > 0) generate
@@ -175,7 +185,7 @@ begin
 			--WISHBONE Latches
 			latch : wb_latch
 				generic map(
-					DATA_WIDTH   => CORE_DATA_WIDTH
+					DATA_WIDTH => CORE_DATA_WIDTH
 				)
 				port map(
 					CLK_I => CLK_I,
