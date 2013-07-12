fa4c05c9d3c5a5de162a2bec77e843be18712f7e
Matthew Bridges <matthewbridges88@gmail.com>
2013-07-12 17:46:28 +0200

    Adds a new core WB_Multi_Latch. This core allows routing random signals into WB_latches and out to the wishbone bus.

---
+++ DUGONG_IP_CORE_Lib/wb_multi_latch/rtl/wb_multi_latch.vhd
@@ -0,0 +1,153 @@
+--
+-- _______/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\________
+-- \ ____/\\\///////\\\____\/\\\_______\/\\\___\/////\\\///____\/\\\\\\___\/\\\_______/\\\///\\\_____\
+--  \ ___\/\\\_____\/\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\/\\\__\/\\\_____/\\\/__\///\\\___\
+--   \ ___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\
+--    \ ___\/\\\//////\\\_____\/\\\/////////\\\_______\/\\\_______\/\\\\//\\\\/\\\___\/\\\_______\/\\\__\
+--     \ ___\/\\\____\//\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\_\//\\\/\\\___\//\\\______/\\\___\
+--      \ ___\/\\\_____\//\\\___\/\\\_______\/\\\_______\/\\\_______\/\\\__\//\\\\\\____\///\\\__/\\\_____\
+--       \ ___\/\\\______\//\\\__\/\\\_______\/\\\____/\\\\\\\\\\\___\/\\\___\//\\\\\______\///\\\\\/______\
+--        \ ___\///________\///___\///________\///____\///////////____\///_____\/////_________\/////________\
+--         \ __________________________________________\          \__________________________________________\
+--          |:------------------------------------------|: DUGONG :|-----------------------------------------:|
+--         / ==========================================/          /========================================= /
+--        / =============================================================================================== /
+--       / ================  Reconfigurable Hardware Interface for computatioN and radiO  ================ /
+--      / ===============================  http://www.rhinoplatform.org  ================================ /
+--     / =============================================================================================== /
+--
+---------------------------------------------------------------------------------------------------------------
+-- Company:		UNIVERSITY OF CAPE TOWN
+-- Engineer: 		MATTHEW BRIDGES
+--
+-- Name:		WB_MULTI_LATCH (008)
+-- Type:		CORE (3)
+-- Description: 	
+--
+-- Compliance:		DUGONG V0.3
+-- ID:			x 0-3-3-008
+---------------------------------------------------------------------------------------------------------------
+--	ADDR	| NAME		| Type		--
+--	0	| LATCH_D[0]	| WB_LATCH	--
+-- 	1	| LATCH_D[1]	| WB_LATCH	--
+-- 	2	| LATCH_D[2]	| WB_LATCH	--
+-- 	3	| LATCH_D[3]	| WB_LATCH	--
+--------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+library DUGONG_PRIMITIVES_Lib;
+use DUGONG_PRIMITIVES_Lib.dprimitives.ALL;
+
+entity wb_multi_latch is
+	generic(
+		CORE_DATA_WIDTH : natural := 32;
+		CORE_ADDR_WIDTH : natural := 3
+	);
+	port(
+		--System Control Inputs
+		CLK_I   : in  STD_LOGIC;
+		RST_I   : in  STD_LOGIC;
+		--Wishbone Slave Lines
+		ADR_I   : in  STD_LOGIC_VECTOR(CORE_ADDR_WIDTH - 1 downto 0);
+		DAT_O   : out STD_LOGIC_VECTOR(CORE_DATA_WIDTH - 1 downto 0);
+		STB_I   : in  STD_LOGIC;
+		ACK_O   : out STD_LOGIC;
+		CYC_I   : in  STD_LOGIC;
+		--LATCH Inputs
+		LATCH_D : in  DWORD_vector(3 downto 0)
+	);
+end entity wb_multi_latch;
+
+architecture Behavioral of wb_multi_latch is
+	subtype small_int is integer range 0 to 2 ** CORE_ADDR_WIDTH - 5;
+	type t is array (0 to (2 ** CORE_ADDR_WIDTH) - 5) of small_int;
+
+	---------------------------------
+	--DEFINE MEMORY STRUCTURE HERE --
+	---------------------------------
+	constant NUMBER_OF_REGISTERS : natural := 0;
+	constant NUMBER_OF_FIFOS     : natural := 0;
+	constant user_addr           : t       := (0, 1, 2, 3); --Addresses of registers followed by Addresses of FIFOs followed by Addresses of Latches
+	--------------------------------------------------
+	--	ADDR	| NAME		| Type		--
+	--	0	| LATCH_D[0]	| WB_LATCH	--
+	-- 	1	| LATCH_D[1]	| WB_LATCH	--
+	-- 	2	| LATCH_D[2]	| WB_LATCH	--
+	-- 	3	| LATCH_D[3]	| WB_LATCH	--
+	--------------------------------------------------
+
+	----------------------------------------
+	--END OF MEMEORY STRUCTURE DEFINITION --
+	----------------------------------------		
+
+	--User memory architecture
+	type ram_type is array (0 to (2 ** CORE_ADDR_WIDTH) - 5) of std_logic_vector(CORE_DATA_WIDTH - 1 downto 0);
+	signal user_D   : ram_type                                                := (others => (others => '0'));
+	signal user_Q   : ram_type                                                := (others => (others => '0'));
+	signal user_stb : std_logic_vector(((2 ** CORE_ADDR_WIDTH) - 5) downto 0) := (others => '0');
+	signal user_ack : std_logic_vector(((2 ** CORE_ADDR_WIDTH) - 5) downto 0) := (others => '0');
+
+	signal wb_addr : small_int;
+
+begin
+	---------------------------------
+	----------{ BUS LOGIC }----------
+	---------------------------------
+
+	--User Memory Address space is equals from 4 up to IP Address(core_addr_width-1:0)
+	addr_generate : if (CORE_ADDR_WIDTH /= 3) generate
+	begin
+		-- Account for offset of 4 due to wb_s status registers
+		wb_addr <= to_integer(unsigned(ADR_I) - 4);
+	end generate addr_generate;
+
+	addr_generate_2 : if (CORE_ADDR_WIDTH = 3) generate
+	begin
+		-- Account for offset of 4 due to wb_s status registers
+		wb_addr <= to_integer(unsigned(ADR_I(CORE_ADDR_WIDTH - 2 downto 0))) when (ADR_I(CORE_ADDR_WIDTH - 1) = '1') else 0;
+	end generate addr_generate_2;
+
+	--Generate WB Latches
+	user_latches : if ((2 ** CORE_ADDR_WIDTH) - 4) /= NUMBER_OF_REGISTERS + NUMBER_OF_FIFOS generate
+	begin
+		user_latches : for i in NUMBER_OF_REGISTERS + NUMBER_OF_FIFOS to ((2 ** CORE_ADDR_WIDTH) - 5) generate
+		begin
+			--WISHBONE Latches
+			latch : wb_latch
+				generic map(
+					DATA_WIDTH => CORE_DATA_WIDTH
+				)
+				port map(
+					CLK_I => CLK_I,
+					RST_I => RST_I,
+					DAT_I => user_D(user_addr(i)),
+					DAT_O => user_Q(user_addr(i)),
+					STB_I => user_stb(user_addr(i)),
+					ACK_O => user_ack(user_addr(i))
+				);
+		end generate user_latches;
+	end generate user_latches;
+
+	--Generate user Strobe lines
+	user_registers_control : for i in 0 to ((2 ** CORE_ADDR_WIDTH) - 5) generate
+	begin
+		--Check for valid addr
+		user_stb(i) <= (STB_I and CYC_I) when (wb_addr = i) else '0';
+	end generate user_registers_control;
+
+	DAT_O <= user_Q(wb_addr);
+	ACK_O <= user_ack(wb_addr);
+
+	----------------------------------
+	----------{ USER LOGIC }----------
+	----------------------------------
+
+	latch_Ds : for i in 0 to 3 generate
+	begin
+		user_D(i) <= LATCH_D(i);
+	end generate latch_Ds;
+
+end architecture Behavioral;
---
+++ DUGONG_IP_CORE_Lib/wb_multi_latch/rtl/wb_multi_latch_ip.vhd
@@ -0,0 +1,131 @@
+--
+-- _______/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\________
+-- \ ____/\\\///////\\\____\/\\\_______\/\\\___\/////\\\///____\/\\\\\\___\/\\\_______/\\\///\\\_____\
+--  \ ___\/\\\_____\/\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\/\\\__\/\\\_____/\\\/__\///\\\___\
+--   \ ___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\
+--    \ ___\/\\\//////\\\_____\/\\\/////////\\\_______\/\\\_______\/\\\\//\\\\/\\\___\/\\\_______\/\\\__\
+--     \ ___\/\\\____\//\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\_\//\\\/\\\___\//\\\______/\\\___\
+--      \ ___\/\\\_____\//\\\___\/\\\_______\/\\\_______\/\\\_______\/\\\__\//\\\\\\____\///\\\__/\\\_____\
+--       \ ___\/\\\______\//\\\__\/\\\_______\/\\\____/\\\\\\\\\\\___\/\\\___\//\\\\\______\///\\\\\/______\
+--        \ ___\///________\///___\///________\///____\///////////____\///_____\/////_________\/////________\
+--         \ __________________________________________\          \__________________________________________\
+--          |:------------------------------------------|: DUGONG :|-----------------------------------------:|
+--         / ==========================================/          /========================================= /
+--        / =============================================================================================== /
+--       / ================  Reconfigurable Hardware Interface for computatioN and radiO  ================ /
+--      / ===============================  http://www.rhinoplatform.org  ================================ /
+--     / =============================================================================================== /
+--
+---------------------------------------------------------------------------------------------------------------
+-- Company:		UNIVERSITY OF CAPE TOWN
+-- Engineer: 		MATTHEW BRIDGES
+--
+-- Name:		WB_MULTI_LATCH_IP (008)
+-- Type:		IP_CORE (4)
+-- Description: 	
+--
+-- Compliance:		DUGONG V0.3
+-- ID:			x 0-3-4-008
+---------------------------------------------------------------------------------------------------------------
+--	ADDR	| NAME		| Type		--
+--	0	| BASE_ADDR	| WB_LATCH	--
+-- 	1	| HIGH_ADDR	| WB_LATCH	--
+-- 	2	| CORE_ID	| WB_LATCH	-- --SEE HEADER
+-- 	3	| xFEDCBA98	| WB_REG	-- --TEST_SIGNAL
+--	4	| LATCH_D[0]	| WB_LATCH	--
+-- 	5	| LATCH_D[1]	| WB_LATCH	--
+-- 	6	| LATCH_D[2]	| WB_LATCH	--
+-- 	7	| LATCH_D[3]	| WB_LATCH	--
+--------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+library DUGONG_PRIMITIVES_Lib;
+use DUGONG_PRIMITIVES_Lib.dprimitives.ALL;
+
+--NB The DATA_WIDTH and ADDR_WIDTH constants are set in the dprimitives package
+entity wb_multi_latch_ip is
+	generic(
+		BASE_ADDR       : UNSIGNED(ADDR_WIDTH + 3 downto 0) := x"00000000";
+		CORE_DATA_WIDTH : NATURAL                           := 32;
+		CORE_ADDR_WIDTH : NATURAL                           := 3
+	);
+	port(
+		--System Control Inputs
+		CLK_I   : in  STD_LOGIC;
+		RST_I   : in  STD_LOGIC;
+		--Slave to WB
+		WB_MS   : in  WB_MS_type;
+		WB_SM   : out WB_SM_type;
+		--LATCH Inputs
+		LATCH_D : in  DWORD_vector(3 downto 0)
+	);
+end entity wb_multi_latch_ip;
+
+architecture RTL of wb_multi_latch_ip is
+	signal adr_i : STD_LOGIC_VECTOR(CORE_ADDR_WIDTH - 1 downto 0);
+	signal dat_o : STD_LOGIC_VECTOR(CORE_DATA_WIDTH - 1 downto 0);
+	signal stb_i : STD_LOGIC;
+	signal ack_o : STD_LOGIC;
+	signal cyc_i : STD_LOGIC;
+
+	component wb_multi_latch is
+		generic(
+			CORE_DATA_WIDTH : natural := 32;
+			CORE_ADDR_WIDTH : natural := 3
+		);
+		port(
+			--System Control Inputs
+			CLK_I   : in  STD_LOGIC;
+			RST_I   : in  STD_LOGIC;
+			--Wishbone Slave Lines
+			ADR_I   : in  STD_LOGIC_VECTOR(CORE_ADDR_WIDTH - 1 downto 0);
+			DAT_O   : out STD_LOGIC_VECTOR(CORE_DATA_WIDTH - 1 downto 0);
+			STB_I   : in  STD_LOGIC;
+			ACK_O   : out STD_LOGIC;
+			CYC_I   : in  STD_LOGIC;
+			--SPI Interface
+			LATCH_D : in  DWORD_vector(3 downto 0)
+		);
+	end component wb_multi_latch;
+begin
+	bus_logic : wb_s
+		generic map(
+			BASE_ADDR       => BASE_ADDR,
+			CORE_ID         => x"00034008", -- SEE HEADER
+			CORE_DATA_WIDTH => CORE_DATA_WIDTH,
+			CORE_ADDR_WIDTH => CORE_ADDR_WIDTH
+		)
+		port map(
+			CLK_I => CLK_I,
+			RST_I => RST_I,
+			WB_MS => WB_MS,
+			WB_SM => WB_SM,
+			ADR_I => adr_i,
+			DAT_I => open,
+			DAT_O => dat_o,
+			WE_I  => open,
+			STB_I => stb_i,
+			ACK_O => ack_o,
+			CYC_I => cyc_i
+		);
+
+	user_logic : wb_multi_latch
+		generic map(
+			CORE_DATA_WIDTH => CORE_DATA_WIDTH,
+			CORE_ADDR_WIDTH => CORE_ADDR_WIDTH
+		)
+		port map(
+			CLK_I   => CLK_I,
+			RST_I   => RST_I,
+			ADR_I   => adr_i,
+			DAT_O   => dat_o,
+			STB_I   => stb_i,
+			ACK_O   => ack_o,
+			CYC_I   => cyc_i,
+			LATCH_D => LATCH_D
+		);
+
+end architecture RTL;
