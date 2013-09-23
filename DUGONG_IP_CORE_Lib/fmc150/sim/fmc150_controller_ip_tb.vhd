471b6705bc22385c4e81ca3478097a748ccd4ae9
Matthew Bridges <matthewbridges88@gmail.com>
2013-09-23 15:44:44 +0200

    Updates fmc150_controller testbench to account for new controller

--- DUGONG_IP_CORE_Lib/fmc150/sim/fmc150_controller_ip_tb.vhd
+++ DUGONG_IP_CORE_Lib/fmc150/sim/fmc150_controller_ip_tb.vhd
@@ -35,41 +35,13 @@ USE IEEE.numeric_std.ALL;
 library DUGONG_PRIMITIVES_Lib;
 use DUGONG_PRIMITIVES_Lib.dprimitives.ALL;
 
+library DUGONG_IP_CORE_Lib;
+use DUGONG_IP_CORE_Lib.dcores.ALL;
+
 entity fmc150_controller_ip_tb IS
 end fmc150_controller_ip_tb;
 
 architecture Behavioral of fmc150_controller_ip_tb is
-
-	-- Component Declaration for the Unit Under Test (UUT)
-	component fmc150_controller_ip
-		generic(
-			BASE_ADDR       : UNSIGNED(ADDR_WIDTH + 3 downto 0) := x"00000000";
-			CORE_DATA_WIDTH : NATURAL                           := 32;
-			CORE_ADDR_WIDTH : NATURAL                           := 4
-		);
-		port(
-			CLK_I          : in  STD_LOGIC;
-			RST_I          : in  STD_LOGIC;
-			WB_MS          : in  WB_MS_type;
-			WB_SM          : out WB_SM_type;
-			SPI_CLK_P_I    : in  STD_LOGIC;
-			SPI_CLK_N_I    : in  STD_LOGIC;
-			SPI_SCLK_O     : out STD_LOGIC;
-			SPI_MOSI_O     : out STD_LOGIC;
-			ADC_MISO_I     : in  STD_LOGIC;
-			ADC_N_SS_O     : out STD_LOGIC;
-			CDC_MISO_I     : in  STD_LOGIC;
-			CDC_N_SS_O     : out STD_LOGIC;
-			DAC_MISO_I     : in  STD_LOGIC;
-			DAC_N_SS_O     : out STD_LOGIC;
-			ADC_RST        : out STD_LOGIC;
-			CDC_REF_EN     : out STD_LOGIC;
-			CDC_N_RST      : out STD_LOGIC;
-			CDC_N_PD       : out STD_LOGIC;
-			CDC_PLL_STATUS : in  STD_LOGIC;
-			DEBUG          : out STD_LOGIC_VECTOR(15 downto 0)
-		);
-	end component fmc150_controller_ip;
 
 	--System Control Inputs:
 	signal CLK_I : std_logic := '0';
@@ -80,52 +52,48 @@ architecture Behavioral of fmc150_controller_ip_tb is
 	signal WB_SM : WB_SM_type;
 
 	--FMC SPI Interface
-	signal SPI_CLK_P_I    : STD_LOGIC := '0';
-	signal SPI_CLK_N_I    : STD_LOGIC := '1';
-	signal SPI_SCLK_O     : STD_LOGIC;
-	signal SPI_MOSI_O     : STD_LOGIC;
-	signal ADC_MISO_I     : STD_LOGIC := '0';
-	signal ADC_N_SS_O     : STD_LOGIC;
-	signal CDC_MISO_I     : STD_LOGIC := '0';
-	signal CDC_N_SS_O     : STD_LOGIC;
-	signal DAC_MISO_I     : STD_LOGIC := '0';
-	signal DAC_N_SS_O     : STD_LOGIC;
-	signal ADC_RST        : STD_LOGIC;
-	signal CDC_REF_EN     : STD_LOGIC;
-	signal CDC_N_RST      : STD_LOGIC;
-	signal CDC_N_PD       : STD_LOGIC;
-	signal CDC_PLL_STATUS : STD_LOGIC;
-	signal DEBUG          : STD_LOGIC_VECTOR(15 downto 0);
+	signal SPI_CLK_P_I : STD_LOGIC                    := '0';
+	signal SPI_CLK_N_I : STD_LOGIC                    := '1';
+	signal SPI_SCLK_O  : STD_LOGIC;
+	signal SPI_MOSI_O  : STD_LOGIC;
+	signal ADC_MISO_I  : STD_LOGIC                    := '0';
+	signal ADC_N_SS_O  : STD_LOGIC;
+	signal CDC_MISO_I  : STD_LOGIC                    := '0';
+	signal CDC_N_SS_O  : STD_LOGIC;
+	signal DAC_MISO_I  : STD_LOGIC                    := '0';
+	signal DAC_N_SS_O  : STD_LOGIC;
+	signal MON_MISO_I  : STD_LOGIC                    := '0';
+	signal MON_N_SS_O  : STD_LOGIC;
+	signal FMC150_GPIO : STD_LOGIC_VECTOR(7 downto 0) := (others => 'Z');
+	signal DEBUG       : STD_LOGIC_VECTOR(31 downto 0);
 
 	-- Clock period definitions
 	constant CLK_I_period     : time := 10 ns;
-	constant SPI_CLK_I_period : time := 320 ns;
+	constant SPI_CLK_I_period : time := 100 ns;
 
 begin
 
 	-- Instantiate the Unit Under Test (UUT)
 	uut : fmc150_controller_ip
 		port map(
-			CLK_I          => CLK_I,
-			RST_I          => RST_I,
-			WB_MS          => WB_MS,
-			WB_SM          => WB_SM,
-			SPI_CLK_P_I    => SPI_CLK_P_I,
-			SPI_CLK_N_I    => SPI_CLK_N_I,
-			SPI_SCLK_O     => SPI_SCLK_O,
-			SPI_MOSI_O     => SPI_MOSI_O,
-			ADC_MISO_I     => ADC_MISO_I,
-			ADC_N_SS_O     => ADC_N_SS_O,
-			CDC_MISO_I     => CDC_MISO_I,
-			CDC_N_SS_O     => CDC_N_SS_O,
-			DAC_MISO_I     => DAC_MISO_I,
-			DAC_N_SS_O     => DAC_N_SS_O,
-			ADC_RST        => ADC_RST,
-			CDC_REF_EN     => CDC_REF_EN,
-			CDC_N_RST      => CDC_N_RST,
-			CDC_N_PD       => CDC_N_PD,
-			CDC_PLL_STATUS => CDC_PLL_STATUS,
-			DEBUG          => DEBUG
+			CLK_I       => CLK_I,
+			RST_I       => RST_I,
+			WB_MS       => WB_MS,
+			WB_SM       => WB_SM,
+			SPI_CLK_P_I => SPI_CLK_P_I,
+			SPI_CLK_N_I => SPI_CLK_N_I,
+			SPI_SCLK_O  => SPI_SCLK_O,
+			SPI_MOSI_O  => SPI_MOSI_O,
+			ADC_MISO_I  => ADC_MISO_I,
+			ADC_N_SS_O  => ADC_N_SS_O,
+			CDC_MISO_I  => CDC_MISO_I,
+			CDC_N_SS_O  => CDC_N_SS_O,
+			DAC_MISO_I  => DAC_MISO_I,
+			DAC_N_SS_O  => DAC_N_SS_O,
+			MON_MISO_I  => MON_MISO_I,
+			MON_N_SS_O  => MON_N_SS_O,
+			FMC150_GPIO => FMC150_GPIO,
+			DEBUG       => DEBUG
 		);
 
 	-- Clock process definitions
@@ -172,7 +140,7 @@ begin
 		WB_MS <= "000" & x"00000000" & x"0000000"; --NULL
 
 		wait until rising_edge(CLK_I);
-		WB_MS <= "111" & x"FEDC1234" & x"0000004"; --Write xFEDCAB98 to 004
+		WB_MS <= "111" & x"F0000007" & x"0000004"; --Write xFEDCAB98 to 004
 		wait until rising_edge(WB_SM(DATA_WIDTH));
 		wait until rising_edge(CLK_I);
 		WB_MS <= "000" & x"00000000" & x"0000000"; --NULL
@@ -184,18 +152,26 @@ begin
 		WB_MS <= "000" & x"00000000" & x"0000000"; --NULL
 
 		wait until rising_edge(CLK_I);
-		WB_MS <= "111" & x"FEDC8765" & x"000000C"; --Write xFEDCAB98 to 004
+		WB_MS <= "111" & x"FEDC9ABC" & x"000000C"; --Write xFEDCAB98 to 004
 		wait until rising_edge(WB_SM(DATA_WIDTH));
 		wait until rising_edge(CLK_I);
 		WB_MS <= "000" & x"00000000" & x"0000000"; --NULL
 
 		wait until rising_edge(CLK_I);
-		WB_MS <= "111" & x"FEDC4321" & x"0000014"; --Write xFEDCAB98 to 004
+		WB_MS <= "111" & x"FEDCDEF0" & x"0000014"; --Write xFEDCAB98 to 014
+		wait until rising_edge(WB_SM(DATA_WIDTH));
+		wait until rising_edge(CLK_I);
+		WB_MS <= "000" & x"00000000" & x"0000000"; --NULL
+
+		wait until rising_edge(CLK_I);
+		WB_MS <= "111" & x"FEDC0FED" & x"000001C"; --Write xFEDCAB98 to 014
 		wait until rising_edge(WB_SM(DATA_WIDTH));
 		wait until rising_edge(CLK_I);
 		WB_MS <= "000" & x"00000000" & x"0000000"; --NULL
 
 		wait;
 	end process;
+
+	CDC_MISO_I <= SPI_MOSI_O;
 
 end;
