c4ce4ce75e5ffd79fd2e28fe014b5df0ad768b28
Matthew Bridges <matthewbridges88@gmail.com>
2013-11-08 15:57:52 +0200

    Updates test bench to reflect changes made to dac3283_serializer

--- fmc150/sim/dac3283_serializer_tb.vhd
+++ fmc150/sim/dac3283_serializer_tb.vhd
@@ -38,31 +38,34 @@ END dac3283_serializer_tb;
 ARCHITECTURE behavior OF dac3283_serializer_tb IS
 
 	-- Component Declaration for the Unit Under Test (UUT)
-
-	COMPONENT dac3283_serializer
-		PORT(
-			CLK_I      : IN  std_logic;
-			RST_I      : IN  std_logic;
-			CH_A_I     : IN  std_logic_vector(15 downto 0);
-			CH_B_I     : IN  std_logic_vector(15 downto 0);
-			DAC_CLK_I  : IN  std_logic;
-			DAC_DCLK_P : OUT std_logic;
-			DAC_DCLK_N : OUT std_logic;
-			DAC_DATA_P : OUT std_logic_vector(7 downto 0);
-			DAC_DATA_N : OUT std_logic_vector(7 downto 0);
-			FRAME_P    : OUT std_logic;
-			FRAME_N    : OUT std_logic;
-			TXENABLE   : OUT std_logic;
-			DEBUG      : OUT std_logic_vector(15 downto 0)
+	component dac3283_serializer is
+		port(
+			--System Control Inputs
+			CLK_I      : in  STD_LOGIC;
+			RST_I      : in  STD_LOGIC;
+			--Signal Channel Inputs
+			DSP_CLK_I  : in  STD_LOGIC;
+			CH_A_I     : in  STD_LOGIC_VECTOR(15 downto 0);
+			CH_B_I     : in  STD_LOGIC_VECTOR(15 downto 0);
+			-- DAC interface
+			DAC_DCLK_P : out STD_LOGIC;
+			DAC_DCLK_N : out STD_LOGIC;
+			DAC_DATA_P : out STD_LOGIC_VECTOR(7 downto 0);
+			DAC_DATA_N : out STD_LOGIC_VECTOR(7 downto 0);
+			FRAME_P    : out STD_LOGIC;
+			FRAME_N    : out STD_LOGIC;
+			TXENABLE   : out STD_LOGIC;
+		-- Testing
+		IO_TEST_EN : in  STD_LOGIC
 		);
-	END COMPONENT;
+	end component dac3283_serializer;
 
 	--Inputs
-	signal CLK_I  : std_logic                     := '0';
-	signal RST_I  : std_logic                     := '1';
-	signal CH_A_I : std_logic_vector(15 downto 0) := (others => '0');
-	signal CH_B_I : std_logic_vector(15 downto 0) := (others => '0');
-	signal DAC_CLK_I  : std_logic                 := '0';
+	signal CLK_I     : std_logic                     := '0';
+	signal RST_I     : std_logic                     := '1';
+	signal CH_A_I    : std_logic_vector(15 downto 0) := (others => '0');
+	signal CH_B_I    : std_logic_vector(15 downto 0) := (others => '0');
+	signal DSP_CLK_I : std_logic                     := '0';
 
 	--Outputs
 	signal DAC_DCLK_P : std_logic;
@@ -72,30 +75,32 @@ ARCHITECTURE behavior OF dac3283_serializer_tb IS
 	signal FRAME_P    : std_logic;
 	signal FRAME_N    : std_logic;
 	signal TXENABLE   : std_logic;
-	signal DEBUG      : std_logic_vector(15 downto 0);
+	signal IO_TEST_EN : STD_LOGIC := '1';
 
 	-- Clock period definitions
-	constant CLK_I_period : time := 10 ns;
-	constant DAC_CLK_I_period : time := 20 ns;
+	constant CLK_I_period     : time := 10 ns;
+	constant DSP_CLK_I_period : time := 20 ns;
+	
 
 BEGIN
 
 	-- Instantiate the Unit Under Test (UUT)
-	uut : dac3283_serializer PORT MAP(
+	uut : dac3283_serializer
+		port map(
 			CLK_I      => CLK_I,
-			RST_I      => RST_I,
-			CH_A_I     => CH_A_I,
-			CH_B_I     => CH_B_I,
-			DAC_CLK_I  => DAC_CLK_I,
-			DAC_DCLK_P => DAC_DCLK_P,
-			DAC_DCLK_N => DAC_DCLK_N,
-			DAC_DATA_P => DAC_DATA_P,
-			DAC_DATA_N => DAC_DATA_N,
-			FRAME_P    => FRAME_P,
-			FRAME_N    => FRAME_N,
-			TXENABLE   => TXENABLE,
-			DEBUG      => DEBUG
-		);
+			 RST_I      => RST_I,
+			 DSP_CLK_I  => DSP_CLK_I,
+			 CH_A_I     => CH_A_I,
+			 CH_B_I     => CH_B_I,
+			 DAC_DCLK_P => DAC_DCLK_P,
+			 DAC_DCLK_N => DAC_DCLK_N,
+			 DAC_DATA_P => DAC_DATA_P,
+			 DAC_DATA_N => DAC_DATA_N,
+			 FRAME_P    => FRAME_P,
+			 FRAME_N    => FRAME_N,
+			 TXENABLE   => TXENABLE,
+			 IO_TEST_EN => IO_TEST_EN
+			 );
 
 	-- Clock process definitions
 	CLK_I_process : process
@@ -105,14 +110,14 @@ BEGIN
 		CLK_I <= '1';
 		wait for CLK_I_period / 2;
 	end process;
-	
+
 	-- Clock process definitions
-	DAC_CLK_I_process : process
+	DSP_CLK_I_process : process
 	begin
-		DAC_CLK_I <= '0';
-		wait for DAC_CLK_I_period / 2;
-		DAC_CLK_I <= '1';
-		wait for DAC_CLK_I_period / 2;
+		DSP_CLK_I <= '0';
+		wait for DSP_CLK_I_period / 2;
+		DSP_CLK_I <= '1';
+		wait for DSP_CLK_I_period / 2;
 	end process;
 
 	-- Stimulus process
@@ -130,12 +135,12 @@ BEGIN
 		wait until rising_edge(CLK_I);
 		CH_A_I <= x"FF00";
 		CH_B_I <= x"F00F";
---		wait until rising_edge(CLK_I);
---		CH_A_I <= x"FF00";
---		CH_B_I <= x"EE00";
---		wait until rising_edge(CLK_I);
---		CH_A_I <= x"0000";
---		CH_B_I <= x"0000";
+		--		wait until rising_edge(CLK_I);
+		--		CH_A_I <= x"FF00";
+		--		CH_B_I <= x"EE00";
+		--		wait until rising_edge(CLK_I);
+		--		CH_A_I <= x"0000";
+		--		CH_B_I <= x"0000";
 
 		wait;
 	end process;
