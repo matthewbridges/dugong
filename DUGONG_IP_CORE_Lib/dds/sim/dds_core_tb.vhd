6a03b9c024ef75f69efa03a82298c98bec8f1256
Matthew Bridges <matthewbridges88@gmail.com>
2013-12-03 14:34:04 +0200

    Replaces old testbench for DDS

--- DUGONG_IP_CORE_Lib/dds/sim/dds_core_tb.vhd
+++ DUGONG_IP_CORE_Lib/dds/sim/dds_core_tb.vhd
@@ -1,97 +1,117 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
 --
--- Create Date:   16:36:14 09/05/2012
--- Design Name:   
--- Module Name:   /home/mbridges/Projects/Dugong/dds_core_tb.vhd
--- Project Name:  Dugong
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: dds_core
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
+-- _______/\\\\\\\\\_______/\\\________/\\\____/\\\\\\\\\\\____/\\\\\_____/\\\_________/\\\\\________
+-- \ ____/\\\///////\\\____\/\\\_______\/\\\___\/////\\\///____\/\\\\\\___\/\\\_______/\\\///\\\_____\
+--  \ ___\/\\\_____\/\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\/\\\__\/\\\_____/\\\/__\///\\\___\
+--   \ ___\/\\\\\\\\\\\/_____\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\//\\\_\/\\\____/\\\______\//\\\__\
+--    \ ___\/\\\//////\\\_____\/\\\/////////\\\_______\/\\\_______\/\\\\//\\\\/\\\___\/\\\_______\/\\\__\
+--     \ ___\/\\\____\//\\\____\/\\\_______\/\\\_______\/\\\_______\/\\\_\//\\\/\\\___\//\\\______/\\\___\
+--      \ ___\/\\\_____\//\\\___\/\\\_______\/\\\_______\/\\\_______\/\\\__\//\\\\\\____\///\\\__/\\\_____\
+--       \ ___\/\\\______\//\\\__\/\\\_______\/\\\____/\\\\\\\\\\\___\/\\\___\//\\\\\______\///\\\\\/______\
+--        \ ___\///________\///___\///________\///____\///////////____\///_____\/////_________\/////________\
+--         \ __________________________________________\          \__________________________________________\
+--          |:------------------------------------------|: DUGONG :|-----------------------------------------:|
+--         / ==========================================/          /========================================= /
+--        / =============================================================================================== /
+--       / ================  Reconfigurable Hardware Interface for computatioN and radiO  ================ /
+--      / ===============================  http://www.rhinoplatform.org  ================================ /
+--     / =============================================================================================== /
 --
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+---------------------------------------------------------------------------------------------------------------
+-- Company:		UNIVERSITY OF CAPE TOWN
+-- Engineer: 		MATTHEW BRIDGES
+--
+-- Name:		GPIO_CONTROLLER_TB
+-- Type:		TB (15)
+-- Description:
+--
+-- Compliance:		DUGONG V1.4
+-- ID:			x 1-4-F
+---------------------------------------------------------------------------------------------------------------
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
 
-ENTITY dds_core_tb IS
-END dds_core_tb;
+use STD.textio.ALL;
+
+entity dds_core_tb is
+	generic(
+		CORE_DATA_WIDTH : NATURAL := 16;
+		CORE_ADDR_WIDTH : NATURAL := 3
+	);
+end dds_core_tb;
 
 ARCHITECTURE behavior OF dds_core_tb IS
 
-	component sink_file
-		generic(stim_file : string := "file_io.out");
-		port(clk  : in STD_LOGIC;
-			 data : in STD_LOGIC_VECTOR(11 downto 0));
-	end component sink_file;
-
 	-- Component Declaration for the Unit Under Test (UUT)
 	component dds_core
-		generic(DATA_WIDTH : natural := 16;
-			    ADDR_WIDTH : natural := 4);
-		port(CLK_I : in  STD_LOGIC;
-			 RST_I : in  STD_LOGIC;
-			 DAT_I : in  STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
-			 DAT_O : out STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0);
-			 ADR_I : in  STD_LOGIC_VECTOR(ADDR_WIDTH - 1 downto 0);
-			 STB_I : in  STD_LOGIC;
-			 WE_I  : in  STD_LOGIC;
-			 ACK_O : out STD_LOGIC);
+		generic(
+			CORE_DATA_WIDTH : natural := 16;
+			CORE_ADDR_WIDTH : natural := 3
+		);
+		port(
+			--System Control Inputs
+			CLK_I     : in  STD_LOGIC;
+			RST_I     : in  STD_LOGIC;
+			--Wishbone Slave Lines
+			ADR_I     : in  STD_LOGIC_VECTOR(CORE_ADDR_WIDTH - 1 downto 0);
+			DAT_I     : in  STD_LOGIC_VECTOR(CORE_DATA_WIDTH - 1 downto 0);
+			DAT_O     : out STD_LOGIC_VECTOR(CORE_DATA_WIDTH - 1 downto 0);
+			WE_I      : in  STD_LOGIC;
+			STB_I     : in  STD_LOGIC;
+			ACK_O     : out STD_LOGIC;
+			CYC_I     : in  STD_LOGIC;
+			--Signal Channel Inputs
+			DSP_CLK_I : in  STD_LOGIC;
+			CH_A_O    : out STD_LOGIC_VECTOR(15 downto 0);
+			CH_B_O    : out STD_LOGIC_VECTOR(15 downto 0)
+		);
 	end component dds_core;
 
-	--Inputs
-	signal CLK_I : std_logic                     := '0';
-	signal RST_I : std_logic                     := '1';
-	signal DAT_I : std_logic_vector(11 downto 0) := (others => '0');
-	signal ADR_I : std_logic_vector(3 downto 0)  := (others => '0');
-	signal STB_I : std_logic                     := '0';
-	signal WE_I  : std_logic                     := '0';
+	--System Control Inputs:
+	signal CLK_I : std_logic := '0';
+	signal RST_I : std_logic := '1';
 
-	--Outputs
-	signal DAT_O : std_logic_vector(11 downto 0);
-	signal ACK_O : std_logic;
+	--Wishbone Slave interface
+	signal ADR_I : STD_LOGIC_VECTOR(CORE_ADDR_WIDTH - 1 downto 0) := (others => '0');
+	signal DAT_I : STD_LOGIC_VECTOR(CORE_DATA_WIDTH - 1 downto 0) := (others => '0');
+	signal DAT_O : STD_LOGIC_VECTOR(CORE_DATA_WIDTH - 1 downto 0);
+	signal WE_I  : STD_LOGIC                                      := '0';
+	signal STB_I : STD_LOGIC                                      := '0';
+	signal ACK_O : STD_LOGIC                                      := '0';
+	signal CYC_I : STD_LOGIC                                      := '0';
+
+	--DSP Signals
+	signal DSP_CLK_I : STD_LOGIC := '0';
+	signal CH_A_O    : STD_LOGIC_VECTOR(15 downto 0);
+	signal CH_B_O    : STD_LOGIC_VECTOR(15 downto 0);
 
 	-- Clock period definitions
-	constant CLK_I_period : time := 10 ns;
+	constant CLK_I_period     : time := 10 ns;
+	constant DSP_CLK_I_period : time := 10 ns;
 
 BEGIN
 
-	sink : sink_file
-		port map(
-			clk  => CLK_I,
-			data => DAT_O
-		);
-
 	-- Instantiate the Unit Under Test (UUT)
 	uut : dds_core
-		generic map(DATA_WIDTH => 12,
-			        ADDR_WIDTH => 4)
-		port map(CLK_I => CLK_I,
-			     RST_I => RST_I,
-			     DAT_I => DAT_I,
-			     DAT_O => DAT_O,
-			     ADR_I => ADR_I,
-			     STB_I => STB_I,
-			     WE_I  => WE_I,
-			     ACK_O => ACK_O);
+		generic map(
+			CORE_DATA_WIDTH => CORE_DATA_WIDTH,
+			CORE_ADDR_WIDTH => CORE_ADDR_WIDTH
+		)
+		port map(
+			CLK_I     => CLK_I,
+			RST_I     => RST_I,
+			ADR_I     => ADR_I,
+			DAT_I     => DAT_I,
+			DAT_O     => DAT_O,
+			WE_I      => WE_I,
+			STB_I     => STB_I,
+			ACK_O     => ACK_O,
+			CYC_I     => CYC_I,
+			DSP_CLK_I => DSP_CLK_I,
+			CH_A_O    => CH_A_O,
+			CH_B_O    => CH_B_O
+		);
 
 	-- Clock process definitions
 	CLK_I_process : process
@@ -102,6 +122,15 @@ BEGIN
 		wait for CLK_I_period / 2;
 	end process;
 
+	-- Clock process definitions
+	DSP_CLK_I_process : process
+	begin
+		DSP_CLK_I <= '0';
+		wait for DSP_CLK_I_period / 2;
+		DSP_CLK_I <= '1';
+		wait for DSP_CLK_I_period / 2;
+	end process;
+
 	-- Stimulus process
 	stim_proc : process
 	begin
@@ -109,13 +138,64 @@ BEGIN
 		wait for 100 ns;
 
 		RST_I <= '0';
+
 		wait for CLK_I_period * 10;
 
 		-- insert stimulus here
-		STB_I <= '1';
-		ADR_I <= x"C"; 
+		wait until rising_edge(CLK_I);
+		DAT_I <= x"0000";               --Read from PHASE_INCREMENT 
+		ADR_I <= "110";                 --ADDR x3
+		WE_I  <= '0';                   --Read
+		STB_I <= '1';                   --Strobe
+		CYC_I <= '1';
+		wait until rising_edge(ACK_O);
+		wait until rising_edge(CLK_I);
+		STB_I <= '0';                   --NULL
+		DAT_I <= x"0000";
+		wait until rising_edge(CLK_I);
+		DAT_I <= x"4000";               --Write x0001 to PHASE_INCREMENT
+		ADR_I <= "110";                 --ADDR x0
+		WE_I  <= '1';                   --Write
+		STB_I <= '1';                   --Strobe
+		wait until rising_edge(ACK_O);
+		wait until rising_edge(CLK_I);
+		STB_I <= '0';                   --NULL
+		WE_I  <= '0';
+		DAT_I <= x"0000";
+		wait until rising_edge(CLK_I);
+		DAT_I <= x"0000";               --Read from SIN_OUT 
+		ADR_I <= "110";                 --ADDR x1
+		WE_I  <= '0';                   --Read
+		STB_I <= '1';                   --Strobe
+		wait until rising_edge(ACK_O);
+		wait until rising_edge(CLK_I);
+		STB_I <= '0';                   --NULL
+		DAT_I <= x"0000";
 
 		wait;
 	end process;
 
-END;
+	signal_to_text : process
+		file outfile_A : text is out "ch_a_o.csv"; --declare output file
+		file outfile_B : text is out "ch_b_o.csv"; --declare output file
+		variable outlineA : line;       --line number declaration 
+		variable outlineB : line;       --line number declaration
+		variable time     : integer := 0;
+	begin
+		wait until rising_edge(DSP_CLK_I);
+		time := time + 10;
+		if (RST_I = '0') then
+			--write(linenumber,value(integer),justified(side),field(width),digits(natural));
+			write(outlineA, to_integer(signed(CH_A_O)), left);
+			write(outlineB, to_integer(signed(CH_B_O)), left);
+			write(outlineA, ", ");
+			write(outlineB, ", ");
+			write(outlineA, time, left);
+			write(outlineB, time, left);
+			-- write line to external file.
+			writeline(outfile_A, outlineA);
+			writeline(outfile_B, outlineB);
+		end if;
+	end process signal_to_text;
+
+end;
