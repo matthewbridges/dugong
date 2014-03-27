39ddc3476c58ab54aaa5f1003cd537f214f8c95d
Matthew Bridges <matthewbridges88@gmail.com>
2014-03-27 16:33:32 +0200

    Applies significan changes to the dds cosimulation script

--- DUGONG_IP_CORE_Lib/dds/matlab/dds_cosimulation.m
+++ DUGONG_IP_CORE_Lib/dds/matlab/dds_cosimulation.m
@@ -1,3 +1,8 @@
+AMPL_WIDTH = 16;
+PHASE_WIDTH = 16;
+
+FULL_SCALE = 2^(AMPL_WIDTH-1) - 1;
+
 %Collect file data
 dds_sim = csvread('dds_simulation_out.csv'); 
 %Extract file data
@@ -6,61 +11,59 @@ x_cos_out = dds_sim(:,2);
 t_out = dds_sim(:,3);
 t_out = t_out* 1e-12; %convert from pico seconds
 
-N = length(t_out)
-sim_time = t_out(N);
+sim_length = length(t_out)
+sim_time = t_out(sim_length)
+
+%Create Simulation timescale
+N = 30000;
+
 Ts = t_out(2);
+t = t_out(1:N);
+
 Fs = 1/Ts;
-
-next_N2 = nextpow2(N);
-N2 = 2^(next_N2 - 1)
-
-t = t_out(1:N2);
-
-AMPL_WIDTH = 16;
-PHASE_WIDTH = 16;
-
-FULL_SCALE = 2^(AMPL_WIDTH-1) - 1;
-
-x_sin_out_floating = x_sin_out(1:N2)/FULL_SCALE;
-x_cos_out_floating = x_cos_out(1:N2)/FULL_SCALE;
-
-figure(1); plot(t, x_sin_out_floating, t, x_cos_out_floating);
-xlabel('Time [seconds]'); ylabel('Amplitude [FS]');
-
-PHASE_INCREMENT = 3*16^3;
-
+finc = Fs/N;
+f = 0:finc:(N/2)*finc;
 fundamental_frequency = Fs/(2^PHASE_WIDTH);
 
-frequency = fundamental_frequency*PHASE_INCREMENT
+x = x_sin_out(1:N)';
+y = x_cos_out(1:N)';
 
-x_sin_gold = 1e-32*randn(N2,1) + sin(2*pi*frequency*t);
+t_plot = 0.000005;
+N_plot = round(t_plot/Ts);
 
-x_error = x_sin_gold - x_sin_out_floating;
-figure(2); plot(t, x_error);
+figure(1); plot(t(1:N_plot), x(1:N_plot)/FULL_SCALE, t(1:N_plot), y(1:N_plot)/FULL_SCALE);
 xlabel('Time [seconds]'); ylabel('Amplitude [FS]');
 
-%Spectral Analysis
-X_sin_out = fft(x_sin_out_floating);
-X_sin_out_Ampl = (1/N2).*(abs(X_sin_out(1:N2/2+1)));
-X_sin_out_Ampl(2:N2/2) = 2*X_sin_out_Ampl(2:N2/2);
-X_sin_out_Ampl_dB = 10*log10(X_sin_out_Ampl);
+han_window = 2*hanning(N);
+han_window = han_window';
 
-X_sin_gold = fft(x_sin_gold);
-X_sin_gold_Ampl = (1/N2).*(abs(X_sin_gold(1:N2/2+1)));
-X_sin_gold_Ampl(2:N2/2) = 2*X_sin_gold_Ampl(2:N2/2);
-X_sin_gold_Ampl_dB = 10*log10(X_sin_gold_Ampl);
+%Signal Spectral Analysis
+X = fft((x/FULL_SCALE).*han_window);
+X_Ampl = abs(X)./N;
+X_Ampl(2:N/2) = 2*X_Ampl(2:N/2);
+X_Ampl_dBFS = 20*log10(X_Ampl);
 
-f = Fs*(0:1/N2:0.5);
+[max_value, max_index] = max(X_Ampl_dBFS);
 
-figure(3); plot(f, X_sin_out_Ampl_dB, f, X_sin_gold_Ampl_dB);
-xlabel('Frequency [Hz]'); ylabel('Ampitude [dBFS]');
+frequency = (max_index-1)*finc
 
-X_error = fft(x_error);
-X_error_Ampl = (1/N2).*(abs(X_error(1:N2/2+1)));
-X_error_Ampl(2:N2/2) = 2*X_error_Ampl(2:N2/2);
-X_error_Ampl_dB = 10*log10(X_error_Ampl);
+PHASE_INCREMENT = round(frequency/fundamental_frequency)
 
-figure(4); plot(f, X_sin_out_Ampl_dB, f, X_error_Ampl_dB);
-xlabel('Frequency [Hz]'); ylabel('Ampitude [dBFS]');
+x_gold_dds = dds(PHASE_INCREMENT, 0, N);
 
-%periodogram(x_sin_out(1:N2),rectwin(N2),N2,Fs);
+x_gold = sin(2*pi*frequency*t);
+
+figure(2); plot(t(1:N_plot), x(1:N_plot)/FULL_SCALE, t(1:N_plot), x_gold(1:N_plot));
+xlabel('Time [seconds]'); ylabel('Amplitude [FS]');
+
+x_error = (x_gold_dds - x);
+max(x_error)
+mean(x_error)
+
+figure(3); plot(t(1:N_plot), x_error(1:N_plot));
+xlabel('Time [seconds]'); ylabel('Error [LSB]');
+
+N_plot = N/2;
+figure(4); plot(f(1:N_plot)./1e6, X_Ampl_dBFS(1:N_plot));
+xlabel('Frequency [MHz]'); ylabel('Amplitude [dB / dBFS]');
+title('15MHz tone compaarison of custom Fixed-point DDS function vs Matlab Floating-point sin function','FontWeight','bold');
