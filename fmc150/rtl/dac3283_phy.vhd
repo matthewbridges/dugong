aa1580681f408f80a8d860aa7abbba5816497441
Matthew Bridges <matthewbridges88@gmail.com>
2016-08-20 19:04:03 +0200

    Upgrades DAC PHY module to an AXI stream module. This is Hardware Verified. Signal integrity test is still required

--- fmc150/rtl/dac3283_phy.vhd
+++ fmc150/rtl/dac3283_phy.vhd
@@ -41,40 +41,38 @@ use DUGONG_PRIMITIVES_Lib.dprimitives.ALL;
 
 entity dac3283_phy is
 	generic(
-		NUMBER_OF_SAMPLES : natural := 4
+		C_S_AXIS_TDATA_WIDTH : integer := 16
 	);
 	port(
 		--System Control Inputs
-		RST_I         : in  STD_LOGIC;
+		RST_I          : in  STD_LOGIC;
 		--DSP Packet Signals
-		DSP_CLK_O     : out STD_LOGIC;
-		DSP_CLK_DIV_O : out STD_LOGIC;
-		DAC_READY     : out STD_LOGIC;
-		CH_A_PACKET_I : in  STD_LOGIC_VECTOR((14 * NUMBER_OF_SAMPLES) - 1 downto 0);
-		CH_A_EN_I     : in  STD_LOGIC;
-		CH_A_VALID_I  : in  STD_LOGIC;
-		CH_B_PACKET_I : in  STD_LOGIC_VECTOR((14 * NUMBER_OF_SAMPLES) - 1 downto 0);
-		CH_B_EN_I     : in  STD_LOGIC;
-		CH_B_VALID_I  : in  STD_LOGIC;
+		DSP_CLK_O      : out STD_LOGIC;
+		DSP_CLK_DIV_O  : out STD_LOGIC;
+		-- AXI4Stream sink: Clock
+		AXIS_ACLK      : in  std_logic;
+		AXIS_ARESETN   : in  std_logic;
+		S0_AXIS_TREADY : out std_logic;
+		S0_AXIS_TDATA  : in  std_logic_vector(C_S_AXIS_TDATA_WIDTH - 1 downto 0);
+		S0_AXIS_TVALID : in  std_logic;
+		S1_AXIS_TREADY : out std_logic;
+		S1_AXIS_TDATA  : in  std_logic_vector(C_S_AXIS_TDATA_WIDTH - 1 downto 0);
+		S1_AXIS_TVALID : in  std_logic;
 		-- DAC interface
-		FMC150_CLK    : in  STD_LOGIC;
-		DAC_DCLK_P    : out STD_LOGIC;
-		DAC_DCLK_N    : out STD_LOGIC;
-		DAC_DATA_P    : out STD_LOGIC_VECTOR(7 downto 0);
-		DAC_DATA_N    : out STD_LOGIC_VECTOR(7 downto 0);
-		FRAME_P       : out STD_LOGIC;
-		FRAME_N       : out STD_LOGIC;
+		FMC150_CLK     : in  STD_LOGIC;
+		DAC_DCLK_P     : out STD_LOGIC;
+		DAC_DCLK_N     : out STD_LOGIC;
+		DAC_DATA_P     : out STD_LOGIC_VECTOR(7 downto 0);
+		DAC_DATA_N     : out STD_LOGIC_VECTOR(7 downto 0);
+		FRAME_P        : out STD_LOGIC;
+		FRAME_N        : out STD_LOGIC;
 		-- Testing
-		IO_TEST_EN    : in  STD_LOGIC
+		IO_TEST_EN     : in  STD_LOGIC
 	);
 end entity dac3283_phy;
 
 architecture RTL of dac3283_phy is
-	signal dsp_clk      : std_logic;
-	signal dsp_clk_DIV4 : std_logic;
-
-	signal dac_ch_a : STD_LOGIC_VECTOR(13 downto 0);
-	signal dac_ch_b : STD_LOGIC_VECTOR(13 downto 0);
+	signal dac_ready : std_logic;
 
 	signal dac_16_ch_a : STD_LOGIC_VECTOR(15 downto 0);
 	signal dac_16_ch_b : STD_LOGIC_VECTOR(15 downto 0);
@@ -103,41 +101,38 @@ architecture RTL of dac3283_phy is
 	end component dac3283_serializer;
 
 begin
-	dsp_rx_ch_a : dsp_rx
-		generic map(
-			DATA_WIDTH        => 14,
-			NUMBER_OF_SAMPLES => 4
-		)
-		port map(
-			RST_I         => RST_I,
-			DSP_CLK_I     => dsp_clk,
-			DSP_CLK_DIV_I => dsp_clk_DIV4,
-			DSP_SAMPLE_O  => dac_ch_a,
-			DSP_PACKET_I  => CH_A_PACKET_I
-		);
+	name : process(AXIS_ACLK) is
+	begin
+		if rising_edge(AXIS_ACLK) then
+			if AXIS_ARESETN = '1' then
+				S0_AXIS_TREADY <= '0';
+				S1_AXIS_TREADY <= '0';
+			else
+				S0_AXIS_TREADY <= dac_ready;
+				S1_AXIS_TREADY <= dac_ready;
+			end if;
 
-	dsp_rx_ch_b : dsp_rx
-		generic map(
-			DATA_WIDTH        => 14,
-			NUMBER_OF_SAMPLES => 4
-		)
-		port map(
-			RST_I         => RST_I,
-			DSP_CLK_I     => dsp_clk,
-			DSP_CLK_DIV_I => dsp_clk_DIV4,
-			DSP_SAMPLE_O  => dac_ch_b,
-			DSP_PACKET_I  => CH_B_PACKET_I
-		);
+			dac_16_ch_a(C_S_AXIS_TDATA_WIDTH - 1 downto 0) <= S0_AXIS_TDATA;
+			dac_16_ch_b(C_S_AXIS_TDATA_WIDTH - 1 downto 0) <= S1_AXIS_TDATA;
 
-	dac_16_ch_a <= dac_ch_a & dac_ch_a(13) & dac_ch_a(13);
-	dac_16_ch_b <= dac_ch_b & dac_ch_b(13) & dac_ch_b(13);
+		--dac_16_ch_a(C_S_AXIS_TDATA_WIDTH - 1 downto 0) <= S0_AXIS_TDATA(C_S_AXIS_TDATA_WIDTH - 1 downto 0);
+		--dac_16_ch_a(C_S_AXIS_TDATA_WIDTH)              <= S0_AXIS_TDATA(C_S_AXIS_TDATA_WIDTH - 1);
+		--dac_16_ch_a(C_S_AXIS_TDATA_WIDTH + 1)          <= S0_AXIS_TDATA(C_S_AXIS_TDATA_WIDTH - 1);
+		--dac_16_ch_b(C_S_AXIS_TDATA_WIDTH - 1 downto 0) <= S1_AXIS_TDATA(C_S_AXIS_TDATA_WIDTH - 1 downto 0);
+		--dac_16_ch_b(C_S_AXIS_TDATA_WIDTH)              <= S1_AXIS_TDATA(C_S_AXIS_TDATA_WIDTH - 1);
+		--dac_16_ch_b(C_S_AXIS_TDATA_WIDTH + 1)          <= S1_AXIS_TDATA(C_S_AXIS_TDATA_WIDTH - 1);
+		end if;
+	end process name;
+
+	--dac_16_ch_a <= dac_ch_a & dac_ch_a(13) & dac_ch_a(13);
+	--dac_16_ch_b <= dac_ch_b & dac_ch_b(13) & dac_ch_b(13);
 
 	dac : dac3283_serializer
 		port map(
 			RST_I          => RST_I,
-			DAC_CLK_O      => dsp_clk,
-			DAC_CLK_DIV4_O => dsp_clk_DIV4,
-			DAC_READY      => DAC_READY,
+			DAC_CLK_O      => DSP_CLK_O,
+			DAC_CLK_DIV4_O => DSP_CLK_DIV_O,
+			DAC_READY      => dac_ready,
 			CH_C_I         => dac_16_ch_a,
 			CH_D_I         => dac_16_ch_b,
 			FMC150_CLK     => FMC150_CLK,
@@ -149,8 +144,5 @@ begin
 			FRAME_N        => FRAME_N,
 			IO_TEST_EN     => IO_TEST_EN
 		);
-
-	DSP_CLK_O     <= dsp_clk;
-	DSP_CLK_DIV_O <= dsp_clk_DIV4;
 
 end architecture RTL;
